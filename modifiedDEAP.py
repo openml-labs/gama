87ff4eb286230317dcca656a517ae068bc61162e
PGijsbers <p.gijsbers@tue.nl>
2018-03-02 19:21:30 +0100

    Added cxOnePoint as it behaved randomly (due to unordered sets).

--- modifiedDEAP.py
+++ modifiedDEAP.py
@@ -80,3 +80,45 @@ def generate(pset, min_, max_, condition, type_=None):
             for arg in reversed(prim.args):
                 stack.append((depth + 1, arg))
     return expr
+
+from collections import defaultdict
+import random
+__type__ = object
+def cxOnePoint(ind1, ind2):
+    """Randomly select in each individual and exchange each subtree with the
+    point as root between each individual.
+    :param ind1: First tree participating in the crossover.
+    :param ind2: Second tree participating in the crossover.
+    :returns: A tuple of two trees.
+    """
+    if len(ind1) < 2 or len(ind2) < 2:
+        # No crossover on single node tree
+        return ind1, ind2
+
+    # List all available primitive types in each individual
+    types1 = defaultdict(list)
+    types2 = defaultdict(list)
+    if ind1.root.ret == __type__:
+        # Not STGP optimization
+        types1[__type__] = range(1, len(ind1))
+        types2[__type__] = range(1, len(ind2))
+        common_types = [__type__]
+    else:
+        for idx, node in enumerate(ind1[1:], 1):
+            types1[node.ret].append(idx)
+        for idx, node in enumerate(ind2[1:], 1):
+            types2[node.ret].append(idx)
+        common_types = set(types1.keys()).intersection(set(types2.keys()))
+
+    common_types = sorted(common_types, key= lambda x:x.__name__)
+    if len(common_types) > 0:
+        type_ = random.choice(list(common_types))
+
+        index1 = random.choice(types1[type_])
+        index2 = random.choice(types2[type_])
+
+        slice1 = ind1.searchSubtree(index1)
+        slice2 = ind2.searchSubtree(index2)
+        ind1[slice1], ind2[slice2] = ind2[slice2], ind1[slice1]
+
+    return ind1, ind2
