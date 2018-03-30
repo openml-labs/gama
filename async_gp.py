eed1f0f497ee4ec322fd0fe97d86b299278bcad3
Gijsbers <p.gijsbers@tue.nl>
2018-03-30 20:50:13 +0200

    Added a single-process version of the async gp. Still have to clean up.

--- async_gp.py
+++ async_gp.py
@@ -1,12 +1,38 @@
 import multiprocessing as mp
+import threading
+
 #from pathos.helpers.mp.process import Process
 #import pathos.multiprocessing as pathosmp
 
-def evaluator_daemon(input_queue, output_queue, fn):
+def evaluator_daemon(input_queue, output_queue, fn):    
     while True:
         identifier, input_ = input_queue.get()
         output = fn(input_)
         output_queue.put((identifier, output))
+
+def async_ea2(pop, toolbox, X, y, cxpb=0.2, mutpb=0.8, n_evals=300, verbose=True, halloffame=None):
+    P = len(pop)
+    running_pop = []
+        
+    for i in range(n_evals):
+        if i < len(pop):
+            ind = pop[i]
+        else:
+            ind = toolbox.individual()
+        
+        comp_ind = toolbox.compile(ind)
+        fitness = toolbox.evaluate(comp_ind)
+        ind.fitness.values = fitness
+        
+        # Add to population
+        running_pop.append(ind)
+        halloffame.update([ind])
+        
+        # Shrink population if needed        
+        if len(running_pop) > P:
+            running_pop.remove(min(running_pop, key = lambda i: i.fitness.values[0]))
+        
+    return running_pop, None
 
 def async_ea(pop, toolbox, X, y, cxpb=0.2, mutpb=0.8, n_evals=300, verbose=True, halloffame=None):
     mp_manager = mp.Manager()
@@ -29,8 +55,8 @@ def async_ea(pop, toolbox, X, y, cxpb=0.2, mutpb=0.8, n_evals=300, verbose=True,
         comp_ind_map[str(comp_ind)] = ind
         input_queue.put((str(comp_ind), comp_ind))
     
-    for i in range(n_evals):
-        print(i)
+    for i in range(n_evals):        
+        #print(i)
         comp_ind_str, fitness = output_queue.get()
         individual = comp_ind_map[comp_ind_str]
         individual.fitness.values = fitness
@@ -49,6 +75,7 @@ def async_ea(pop, toolbox, X, y, cxpb=0.2, mutpb=0.8, n_evals=300, verbose=True,
         comp_ind_map[str(comp_ind)] = ind
         input_queue.put((str(comp_ind), comp_ind))
     
+    print('done')
     return running_pop, None
         
     
