8a09d6504823e6340e25e662bf29a63e01a4425f
PGijsbers <p.gijsbers@tue.nl>
2018-04-23 17:17:56 +0200

    Changed to accomodate new parameter names.

--- main.py
+++ main.py
@@ -2,30 +2,39 @@
 Created on Thu Jan 11 11:45:30 2018
 @author: Pieter Gijsbers
 """
-
+import sys
+sys.path.append('..\openml-python')
+from openml import tasks
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, mean_squared_error
 from time import time
 
-from gama import Gama
+from GamaRegressor import GamaRegressor
+from GamaClassifier import  GamaClassifier
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
+    mode = 'clf'
     if False:
-        from openml import tasks
-        phoneme_task_id = 145857
+        phoneme_task_id = 219
         task = tasks.get_task(phoneme_task_id)
         X, y = task.get_X_and_y()
         X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, shuffle=True, random_state=42)
-    else:
+    elif True:
         iris = load_iris()
         X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, stratify=iris.target, shuffle=True, random_state=42)
-    
-    for _ in range(5):
+    elif True:
+        moneyball_task = 167148
+        task = tasks.get_task(moneyball_task)
+        X, y = task.get_X_and_y()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, random_state=42)
+        mode = 'reg'
+
+    for _ in range(1):
         start = time()
-        gpaml = Gama(random_state=1, n_generations = 15, pop_size=20)
+        gpaml = GamaClassifier(random_state=1, generations=5, population_size=10, n_jobs=7)
         gpaml.fit(X_train, y_train)
         print('dur', time()-start)
     predictions_1 = gpaml.predict(X_test)
