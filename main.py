d4461d9f6e39602ca907e7704a718b49ab821067
PGijsbers <p.gijsbers@tue.nl>
2018-05-14 17:59:50 +0200

    Example of using Visualizer.

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from sklearn.metrics import accuracy_score, mean_squared_error
 from time import time
 
 from GamaRegressor import GamaRegressor
-from GamaClassifier import  GamaClassifier
-
+from GamaClassifier import GamaClassifier
+from visualization.visualizer import Visualizer
 
 if __name__ == '__main__':
     mode = 'clf'
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     elif True:
         iris = load_iris()
         X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, stratify=iris.target, shuffle=True, random_state=42)
-    elif True:
+    elif False:
         moneyball_task = 167148
         task = tasks.get_task(moneyball_task)
         X, y = task.get_X_and_y()
@@ -34,7 +34,11 @@ if __name__ == '__main__':
 
     for _ in range(1):
         start = time()
-        gpaml = GamaClassifier(random_state=1, generations=5, population_size=10, n_jobs=7)
+        gpaml = GamaClassifier(random_state=1, generations=5, population_size=20, n_jobs=1, async=False)
+        gpaml.generation_completed(lambda x: print('generation completed!', len(x), 'individuals.'))
+        viz = Visualizer()
+        gpaml.evaluation_completed(viz.new_evaluation_result)
+        gpaml._observer.on_pareto_updated(viz.new_pareto_entry)
         gpaml.fit(X_train, y_train)
         print('dur', time()-start)
     predictions_1 = gpaml.predict(X_test)
