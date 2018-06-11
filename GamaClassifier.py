e983ff9617b55bee9dbdb7745070ed3351b8deca
PGijsbers <p.gijsbers@tue.nl>
2018-06-11 15:48:46 +0200

    For classification problems, y may now also be specified as a list or pandas series of string objects. Internal representation and stored predictions will be in encoded format.

--- GamaClassifier.py
+++ GamaClassifier.py
@@ -1,5 +1,6 @@
 import scipy.stats
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 from .gama import Gama
 from .utilities.configuration import clf_config
@@ -11,6 +12,7 @@ class GamaClassifier(Gama):
             config = clf_config
         if not objectives:
             objectives = ('neg_log_loss', 'size')
+        self._label_encoder = None
         super().__init__(*args, **kwargs, config=config, objectives=objectives)
 
     def merge_predictions(self, Y):
@@ -19,4 +21,15 @@ class GamaClassifier(Gama):
 
     def predict(self, X):
         predictions = np.argmax(self.predict_proba(X), axis=1)
-        return np.squeeze(predictions)
+        predictions = np.squeeze(predictions)
+        if self._label_encoder:
+            return np.asarray(self._label_encoder.inverse_transform(predictions))
+        else:
+            return predictions
+
+    def fit(self, X, y, warm_start=False, auto_ensemble_n=10):
+        # Allows y input in list and pandas series form. Multi-label and DataFrame does not work.
+        if isinstance(y[0], str):
+            self._label_encoder = LabelEncoder().fit(y)
+            y = self._label_encoder.transform(y)
+        super().fit(X, y, warm_start, auto_ensemble_n)
