91623600e0d32ae1110bb71201af39d304238a3e
PGijsbers <p.gijsbers@tue.nl>
2018-06-12 12:28:06 +0200

    Work on incremental building of ensemble.

--- utilities/auto_ensemble.py
+++ utilities/auto_ensemble.py
@@ -1,6 +1,5 @@
 """
-TODO: use scikit-learn components to create the ensemble.
-created multiple ensembles from randomly selected subset of models, and average assembles.
+
 """
 
 from collections import namedtuple, Counter
@@ -13,14 +12,13 @@ from sklearn.preprocessing import OneHotEncoder
 
 from ..ea.evaluation import string_to_metric, evaluate, Metric
 
-
 log = logging.getLogger(__name__)
 Model = namedtuple("Model", ['name', 'pipeline', 'predictions'])
 
 
 class Ensemble(object):
 
-    def __init__(self, model_library_directory, metric, y_true, start_size=1):
+    def __init__(self, model_library_directory, metric, y_true):
         """
 
         :param model_library_directory: a directory containing results of model evaluations.
@@ -34,28 +32,80 @@ class Ensemble(object):
         self._model_library_directory = model_library_directory
         self._metric = metric
         self._y_true = y_true
-        self._start_size = start_size
-        self._models = None
+        self._models = []
         self._fit_models = None
+        self._maximize = True
 
-    def build(self, n_models_in_ensemble):
-        """ Constructs an ensemble out of a library of models. """
-        if self._start_size > n_models_in_ensemble:
+    def build(self, n_models_in_ensemble, start_size=1):
+        """ Constructs an ensemble out of a library of models.
+
+        :param n_models_in_ensemble: The number of models to include in the ensemble.
+        :param start_size: The number of n best models to include automatically.
+        :return: self
+        """
+        if start_size > n_models_in_ensemble:
             raise ValueError("`n_models_in_ensemble` cannot be smaller than start_size specified."
-                             "Values were respectively {} and {}".format(n_models_in_ensemble, self._start_size))
+                             "Values were respectively {} and {}".format(n_models_in_ensemble, start_size))
 
         models = load_predictions(self._model_library_directory)
         self._models = build_ensemble(models, self._metric, self._y_true,
-                                      start_size=self._start_size, end_size=n_models_in_ensemble)
+                                      start_size=start_size, end_size=n_models_in_ensemble)
+        return self
+
+    def build_initial_ensemble(self, n):
+        """ Builds an ensemble of n models, based solely on the performance of individual models, not their combined performance.
+
+        :param n: Number of models to include.
+        :return: self
+        """
+        pass
+        if not n > 0:
+            raise ValueError("Ensemble must include at least one model.")
+        if self._models:
+            log.warning("The ensemble already contained models. Overwriting the ensemble.")
+
+        models = load_predictions(self._model_library_directory)
+        sorted_ensembles = sorted(models, key=lambda m: evaluate(self._metric, self._y_true, m.predictions))
+        if self._maximize:
+            sorted_ensembles = reversed(sorted_ensembles)
+        self._models = list(sorted_ensembles)[:n]
+        return self
+
+    def add_models(self, n):
+        """ Adds new models to the ensemble based on earlier given data.
+
+        :param n: Number of models to add to current ensemble.
+        :return: self
+        """
+        if not n > 0:
+            raise ValueError("n must be greater than 0.")
+
+        models = load_predictions(self._model_library_directory)
+        ensemble = self._models
+        best_ensemble = ensemble
+
+        for _ in range(n):
+            best_ensemble_score = -float('inf') if self._maximize else float('inf')
+            for model in models:
+                candidate_ensemble = ensemble + [model]
+                candidate_ensemble_score = evaluate_ensemble(candidate_ensemble, self._metric, self._y_true)
+                if ((self._maximize and best_ensemble_score < candidate_ensemble_score) or
+                        (not self._maximize and best_ensemble_score > candidate_ensemble_score)):
+                    best_ensemble, best_ensemble_score = candidate_ensemble, candidate_ensemble_score
+            ensemble = best_ensemble
+            log.debug('Ensemble size {} , best score: {}'.format(len(ensemble), best_ensemble_score))
+
+        self._models = ensemble
+        return self
 
     def fit(self, X, y):
         """ Constructs an Ensemble out of the library of models.
 
         :param X: Data to fit the final selection of models on.
         :param y: Targets corresponding to features X.
-        :return: None.
+        :return: self.
         """
-        if self._models is None:
+        if not self._models:
             raise RuntimeError("You need to call `build` to select models for the ensemble, before fitting them.")
 
         self._fit_models = [model.pipeline.fit(X, y) for model in self._models]
@@ -94,7 +144,7 @@ class Ensemble(object):
 
     def __str__(self):
         # TODO add internal score and rank of pipeline
-        if self._models is None:
+        if not self._models:
             return "Ensemble with no models."
         components = Counter([m.name for m in self._models])
         ensemble_str = "Ensemble of {} unique pipelines.\nW\tPipeline\n".format(len(components))
@@ -142,7 +192,7 @@ def evaluate_ensemble(ensemble, metric, y_true):
     return evaluate(metric, y_true, average_predictions)
 
 
-def build_ensemble(models, metric, y_true, start_size=0, end_size=5, maximize=True):
+def build_ensemble(models, metric, y_true, start_size=0, end_size=5, maximize=True, consider_top_n=1000):
     """
 
     :param models: list of models
@@ -158,6 +208,7 @@ def build_ensemble(models, metric, y_true, start_size=0, end_size=5, maximize=Tr
 
     sorted_ensembles = sorted(models, key=lambda m: evaluate(metric, y_true, m.predictions))
     sorted_ensembles = reversed(sorted_ensembles) if maximize else sorted_ensembles
+    sorted_ensembles = list(sorted_ensembles)[:consider_top_n]
     ensemble = list(sorted_ensembles)[:start_size]
     best_ensemble = ensemble
 
