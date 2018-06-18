cbd28c8d1d7d768ddbcef4825d0fb1cbf1f331c9
PGijsbers <p.gijsbers@tue.nl>
2018-06-18 15:40:43 +0200

    When cancelling jobs, jobs queued before the cancel will now correctly be ignored when calling  later.

--- utilities/function_dispatcher.py
+++ utilities/function_dispatcher.py
@@ -42,6 +42,8 @@ class FunctionDispatcher(object):
         self._toolbox = toolbox
 
         self._outstanding_job_counter = 0
+        self._outstanding_jobs = []
+        self._outstanding_jobs_to_ignore = []
         self._subscribers = []
         self._job_map = {}
         self._child_processes = []
@@ -59,7 +61,7 @@ class FunctionDispatcher(object):
         comp_ind = self._toolbox.compile(individual)
         self._job_map[str(comp_ind)] = individual
         self._input_queue.put((str(comp_ind), comp_ind))
-        self._outstanding_job_counter += 1
+        self._outstanding_jobs.append(str(comp_ind))
 
     def _get_next_from_daemons(self):
         # If we just used the blocking queue.get, then KeyboardInterrupts/Timeout would not work.
@@ -74,40 +76,62 @@ class FunctionDispatcher(object):
                     time.sleep(0.1)  # seconds
 
                 comp_ind_str, fitness = self._output_queue.get(block=False)
-                return self._job_map[comp_ind_str], fitness
+                return comp_ind_str, fitness
 
             except queue.Empty:
                 last_get_successful = False
                 continue
 
     def get_next_result(self):
-        if self._outstanding_job_counter <= 0:
-            raise ValueError("You have to queue an evaluation for each time you call this function.")
-        else:
-            self._outstanding_job_counter -= 1
+        if len(self._outstanding_jobs) <= 0:
+            raise ValueError("You have to queue an evaluation for each time you call this function since last cancel.")
 
-        if self._n_jobs > 1:
-            return self._get_next_from_daemons()
-        else:
-            # For n_jobs = 1, we do not want to spawn a separate process. Mimic behaviour.
-            identifier, input_ = self._input_queue.get()
-            output = self._evaluate_fn(input_)
-            return self._job_map[identifier], output
+        while True:
+            if self._n_jobs > 1:
+                identifier, output = self._get_next_from_daemons()
+            else:
+                # For n_jobs = 1, we do not want to spawn a separate process. Mimic behaviour.
+                identifier, input_ = self._input_queue.get()
+                output = self._evaluate_fn(input_)
+
+            if identifier in self._outstanding_jobs:
+                self._outstanding_jobs.remove(identifier)
+                return self._job_map[identifier], output
+            elif identifier in self._outstanding_jobs_to_ignore:
+                self._outstanding_jobs_to_ignore.remove(identifier)
+            else:
+                raise ValueError("Found unexpected job.")
 
     def cancel_all_evaluations(self):
-        jobs_cancelled = 0
         while True:
             try:
-                self._input_queue.get(block=False)
-                jobs_cancelled += 1
-            except queue.Empty:
-                self._outstanding_job_counter -= jobs_cancelled
-                if self._outstanding_job_counter > 0:
-                    log.warning("Cancelled {} queued jobs, but {} are already being processed."
-                                .format(jobs_cancelled, self._outstanding_job_counter))
+                identifier, _ = self._input_queue.get(block=False)
+                if identifier in self._outstanding_jobs:
+                    self._outstanding_jobs.remove(identifier)
+                elif identifier in self._outstanding_jobs_to_ignore:
+                    self._outstanding_jobs_to_ignore.remove(identifier)
                 else:
-                    log.info("Cancelled {} jobs, no more jobs being processed.".format(jobs_cancelled))
+                    raise ValueError("Found unexpected job.")
+            except queue.Empty:
+                self._outstanding_jobs_to_ignore += self._outstanding_jobs
+                self._outstanding_jobs = []
                 break
+
+        while True:
+            try:
+                identifier, _ = self._output_queue.get(block=False)
+                if identifier in self._outstanding_jobs_to_ignore:
+                    self._outstanding_jobs_to_ignore.remove(identifier)
+                else:
+                    raise ValueError("Found unexpected job.")
+            except queue.Empty:
+                break
+
+        if len(self._outstanding_jobs_to_ignore) > 0:
+            log.warning("Cancelled queued jobs, but {} are already being processed."
+                        .format(len(self._outstanding_jobs_to_ignore)))
+        else:
+            log.info("Cancelled all jobs, no more jobs being processed.")
 
     def shut_down(self):
         log.info('Shutting down additional processes used for parallel asynchronous evaluations.')
