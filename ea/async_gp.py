1de4c7d10f7dd5ed03d49e96dd0b6d6827a38710
PGijsbers <p.gijsbers@tue.nl>
2018-06-19 17:48:41 +0200

    Changes to make sure no double individuals are generated/evaluated.

--- ea/async_gp.py
+++ ea/async_gp.py
@@ -9,10 +9,13 @@ from ..utilities.function_dispatcher import FunctionDispatcher
 log = logging.getLogger(__name__)
 
 
-def async_ea(objectives, population, toolbox, evaluation_callback=None, restart_callback=None, n_evaluations=10000, n_jobs=1):
+def async_ea(objectives, start_population, toolbox, evaluation_callback=None, restart_callback=None, n_evaluations=10000, n_jobs=1):
     logger = MultiprocessingLogger()
     evaluation_dispatcher = FunctionDispatcher(n_jobs, partial(toolbox.evaluate, logger=logger), toolbox)
-    max_population_size = len(population)
+
+    max_population_size = len(start_population)
+    queued_individuals_str = set()
+    queued_individuals = {}
 
     try:
         evaluation_dispatcher.start()
@@ -21,17 +24,19 @@ def async_ea(objectives, population, toolbox, evaluation_callback=None, restart_
         while restart:
             restart = False
             current_population = []
-            queued_individuals = {}
 
             log.info('Starting EA with new population.')
-            for ind in population:
-                compiled_individual = toolbox.compile(ind)
-                identifier = evaluation_dispatcher.queue_evaluation(compiled_individual)
-                queued_individuals[identifier] = ind
+            for individual in start_population:
+                if str(individual) not in queued_individuals_str:
+                    queued_individuals_str.add(str(individual))
+                    compiled_individual = toolbox.compile(individual)
+                    if compiled_individual is not None:
+                        identifier = evaluation_dispatcher.queue_evaluation(compiled_individual)
+                        queued_individuals[identifier] = individual
 
             for _ in range(n_evaluations):
                 identifier, output, _ = evaluation_dispatcher.get_next_result()
-                individual = queued_individuals.pop(identifier)
+                individual = queued_individuals[identifier]
 
                 score, evaluation_time, length = output
                 if len(objectives) == 1:
@@ -49,7 +54,7 @@ def async_ea(objectives, population, toolbox, evaluation_callback=None, restart_
                 if restart_callback is not None and restart_callback():
                     log.info("Restart criterion met. Restarting with new random population.")
                     restart = True
-                    population = toolbox.population(n=max_population_size)
+                    start_population = toolbox.population(n=max_population_size)
                     break
 
                 current_population.append(individual)
@@ -58,10 +63,17 @@ def async_ea(objectives, population, toolbox, evaluation_callback=None, restart_
                     current_population.remove(to_remove[0])
 
                 if len(current_population) > 1:
-                    new_individual = toolbox.create(current_population, 1)[0]
-                    compiled_individual = toolbox.compile(new_individual)
-                    identifier = evaluation_dispatcher.queue_evaluation(compiled_individual)
-                    queued_individuals[identifier] = new_individual
+                    for _ in range(50):
+                        new_individual = toolbox.create(current_population, 1)[0]
+                        if str(new_individual) not in queued_individuals_str:
+                            queued_individuals_str.add(str(new_individual))
+                            compiled_individual = toolbox.compile(new_individual)
+                            if compiled_individual is not None:
+                                identifier = evaluation_dispatcher.queue_evaluation(compiled_individual)
+                                queued_individuals[identifier] = new_individual
+                                break
+                    else:
+                        log.warning('unable to create new individual.')
 
             evaluation_dispatcher.restart()
     except stopit.utils.TimeoutException:
