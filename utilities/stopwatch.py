b9dd14791210c8375ddd2f4acf5de429d2888a01
PGijsbers <p.gijsbers@tue.nl>
2018-06-20 13:38:37 +0200

    Added _is_running as private because it should not be meddled with from outside.

--- utilities/stopwatch.py
+++ utilities/stopwatch.py
@@ -3,27 +3,27 @@ import time
 
 class Stopwatch:
     """ A context manager that keeps track of wall clock time spent."""
-    __slots__ = ['_start', '_end', 'is_running']
+    __slots__ = ['_start', '_end', '_is_running']
 
     def __init__(self):
-        self.is_running = False
+        self._is_running = False
         self._start = 0
         self._end = 0
 
     def __enter__(self):
-        self.is_running = True
+        self._is_running = True
         self._start = time.time()
         return self
 
     def __exit__(self, *args):
         self._end = time.time()
-        self.is_running = False
+        self._is_running = False
         return False  # do not suppress any exceptions.
 
     @property
     def elapsed_time(self):
         """ If used in with-statement, start since with-statement. If used after, time spent during with statement."""
-        if self.is_running:
+        if self._is_running:
             return time.time() - self._start
         else:
             return self._end - self._start
