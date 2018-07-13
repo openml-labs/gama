eb90cc1af47a72cdfcdf76a1365b11def7fa4291
PGijsbers <p.gijsbers@tue.nl>
2018-07-13 18:18:26 +0200

    Removed merge_predictions as it is not used.

--- GamaClassifier.py
+++ GamaClassifier.py
@@ -15,10 +15,6 @@ class GamaClassifier(Gama):
         self._label_encoder = None
         super().__init__(*args, **kwargs, config=config, objectives=objectives)
 
-    def merge_predictions(self, Y):
-        """ Computes predictions from a matrix of predictions, with predictions from a pipeline in each columns. """
-        return scipy.stats.mode(Y, axis=1)[0]
-
     def predict(self, X):
         predictions = np.argmax(self.predict_proba(X), axis=1)
         predictions = np.squeeze(predictions)
