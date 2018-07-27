502ff51c7a6b9b5d8aa767cb36748d9d602b6c0a
PGijsbers <p.gijsbers@tue.nl>
2018-07-27 17:47:49 +0200

    Refactor. Create some sub-functions to make the core of the algorithm stand out more.

--- ea/async_gp.py
+++ ea/async_gp.py
@@ -10,7 +10,24 @@ from ..utilities.function_dispatcher import FunctionDispatcher
 log = logging.getLogger(__name__)
 
 
-def async_ea(objectives, start_population, toolbox, evaluation_callback=None, restart_callback=None, n_evaluations=10000, max_time_seconds=1e7, n_jobs=1):
+def safe_outside_call(fn, timeout):
+    """ Calls fn and log any exception it raises without reraising, except for TimeoutException. """
+    try:
+        fn()
+    except stopit.utils.TimeoutException:
+        raise
+    except Exception:
+        # We actually want to catch any other exception here, because the callback code can be
+        # arbitrary (it can be provided by users). This excuses the catch-all Exception.
+        # Note that KeyboardInterrupts are not exceptions and get elevated to the caller.
+        log.warning("Exception during callback.", exc_info=True)
+        pass
+    if timeout():
+        log.warning("Time exceeded during callback, but exception was swallowed.")
+        raise stopit.utils.TimeoutException
+
+
+def async_ea(objectives, start_population, toolbox, evaluation_callback=None, restart_callback=None, elimination_callback=None, n_evaluations=10000, max_time_seconds=1e7, n_jobs=1):
     if max_time_seconds <= 0 or max_time_seconds > 3e6:
         raise ValueError("'max_time_seconds' must be greater than 0 and less than or equal to 3e6, but was {}."
                          .format(max_time_seconds))
@@ -18,65 +35,66 @@ def async_ea(objectives, start_population, toolbox, evaluation_callback=None, re
         raise ValueError("'n_evaluations' must be non-negative, but was {}.".format(n_evaluations))
     if n_jobs <= 0:
         raise ValueError("'n_jobs' must be non-negative, but was {}.".format(n_jobs))
-        
+
+    start_time = time.time()
+    max_population_size = len(start_population)
+    queued_individuals_str = set()
+    queued_individuals = {}
+
+    def exceed_timeout():
+        return (time.time() - start_time) > max_time_seconds
+
+    def queue_individual_for_evaluation(individual):
+        """ Place an individual in the queue for evaluation if it compiles and is not yet queued. """
+        if str(individual) not in queued_individuals_str:
+            queued_individuals_str.add(str(individual))
+            compiled_individual = toolbox.compile(individual)
+            if compiled_individual is not None:
+                identifier = evaluation_dispatcher.queue_evaluation(compiled_individual)
+                queued_individuals[identifier] = individual
+                return True
+        return False
+
+    def get_next_evaluation_result():
+        """ Get a new evaluation result, process it and assign it to the correct individual. """
+        identifier, output, _ = evaluation_dispatcher.get_next_result()
+        individual = queued_individuals[identifier]
+        score, evaluation_time, length = output
+        if len(objectives) == 1:
+            individual.fitness.values = (score,)
+        elif objectives[1] == 'time':
+            individual.fitness.values = (score, evaluation_time)
+        elif objectives[1] == 'size':
+            individual.fitness.values = (score, length)
+        individual.fitness.time = evaluation_time
+
+        if n_jobs > 1:
+            logger.flush_to_log(log)
+        return individual
+
     with stopit.ThreadingTimeout(max_time_seconds) as c_mgr:
         logger = MultiprocessingLogger() if n_jobs > 1 else log
         evaluation_dispatcher = FunctionDispatcher(n_jobs, partial(toolbox.evaluate, logger=logger))
-
-        max_population_size = len(start_population)
-        queued_individuals_str = set()
-        queued_individuals = {}
-        start_time = time.time()
-
         evaluation_dispatcher.start()
 
-        restart = True
-        while restart:
-            restart = False
+        should_restart = True
+        while should_restart:
+            should_restart = False
             current_population = []
 
             log.info('Starting EA with new population.')
             for individual in start_population:
-                if str(individual) not in queued_individuals_str:
-                    queued_individuals_str.add(str(individual))
-                    compiled_individual = toolbox.compile(individual)
-                    if compiled_individual is not None:
-                        identifier = evaluation_dispatcher.queue_evaluation(compiled_individual)
-                        queued_individuals[identifier] = individual
+                queue_individual_for_evaluation(individual)
 
             for ind_no in range(n_evaluations):
-                identifier, output, _ = evaluation_dispatcher.get_next_result()
-                individual = queued_individuals[identifier]
-
-                score, evaluation_time, length = output
-                if len(objectives) == 1:
-                    individual.fitness.values = (score,)
-                elif objectives[1] == 'time':
-                    individual.fitness.values = (score, evaluation_time)
-                elif objectives[1] == 'size':
-                    individual.fitness.values = (score, length)
-                individual.fitness.time = evaluation_time
-
-                if n_jobs > 1:
-                    logger.flush_to_log(log)
+                individual = get_next_evaluation_result()
 
                 if evaluation_callback:
-                    try:
-                        evaluation_callback(individual)
-                    except stopit.utils.TimeoutException:
-                        raise
-                    except Exception:
-                        # We actually want to catch any other exception here, because the callback code can be
-                        # arbitrary (it can be provided by users). This excuses the catch-all Exception.
-                        log.warning("Exception during callback.", exc_info=True)
-                        pass
-                    if time.time() - start_time > max_time_seconds:
-                        log.warning("Time exceeded during callback.")
-                        raise stopit.utils.TimeoutException
+                    safe_outside_call(partial(evaluation_callback, individual), exceed_timeout)
 
-                if restart_callback is not None and restart_callback():
+                should_restart = (restart_callback is not None and restart_callback())
+                if should_restart:
                     log.info("Restart criterion met. Restarting with new random population.")
-                    restart = True
                     start_population = toolbox.population(n=max_population_size)
                     break
 
@@ -85,17 +103,14 @@ def async_ea(objectives, start_population, toolbox, evaluation_callback=None, re
                     to_remove = toolbox.eliminate(current_population, 1)
                     log.debug("Removed from population: {}".format(to_remove))
                     current_population.remove(to_remove[0])
+                    if elimination_callback:
+                        safe_outside_call(partial(elimination_callback, to_remove[0]), exceed_timeout)
 
                 if len(current_population) > 1:
                     for _ in range(50):
                         new_individual = toolbox.create(current_population, 1)[0]
-                        if str(new_individual) not in queued_individuals_str:
-                            queued_individuals_str.add(str(new_individual))
-                            compiled_individual = toolbox.compile(new_individual)
-                            if compiled_individual is not None:
-                                identifier = evaluation_dispatcher.queue_evaluation(compiled_individual)
-                                queued_individuals[identifier] = new_individual
-                                break
+                        if queue_individual_for_evaluation(new_individual):
+                            break
                     else:
                         log.warning('unable to create new individual.')
 
