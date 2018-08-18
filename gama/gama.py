24825a1cb990c92ac275f4febef9e9de2169009a
PGijsbers <p.gijsbers@tue.nl>
2018-08-17 19:26:13 -0700

    Removed stopit import.

--- gama/gama.py
+++ gama/gama.py
@@ -11,8 +11,6 @@ import numpy as np
 from deap import base, creator, tools, gp
 from sklearn.preprocessing import Imputer
 
-import stopit
-
 from .ea.modified_deap import cxOnePoint
 from .ea import automl_gp
 from .ea.automl_gp import compile_individual, pset_from_config, generate_valid, random_valid_mutation
