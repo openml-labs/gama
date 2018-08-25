e9e719eadc676d1975f6895909532a3d975bc4d6
PGijsbers <p.gijsbers@tue.nl>
2018-08-25 00:32:22 -0700

    Remove classifiers that do not support probabilities when Metric needs probabilities.

--- gama/GamaClassifier.py
+++ gama/GamaClassifier.py
@@ -1,8 +1,10 @@
+import inspect
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
 from .gama import Gama
 from gama.configuration.classification import clf_config
+from gama.ea.metrics import Metric
 
 
 class GamaClassifier(Gama):
@@ -11,10 +13,21 @@ class GamaClassifier(Gama):
             config = clf_config
         if not objectives:
             objectives = ('neg_log_loss', 'size')
+
+        if Metric(objectives[0]).requires_probabilities:
+            # we don't want classifiers that do not have `predict_proba`, because then we have to
+            # start doing one hot encodings of predictions etc.
+            config = {alg: hp for (alg, hp) in config.items()
+                      if not (inspect.isclass(alg) and not hasattr(alg(), 'predict_proba'))}
+
         self._label_encoder = None
         super().__init__(*args, **kwargs, config=config, objectives=objectives)
 
     def predict(self, X):
+        if self._best_pipeline is not None:
+            return self._best_pipeline.predict(X)
+        raise NotImplementedError
+        # Have to rethink how we do Ensembles.
         predictions = np.argmax(self.predict_proba(X), axis=1)
         predictions = np.squeeze(predictions)
         if self._label_encoder:
