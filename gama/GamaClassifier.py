d7661f46a55ce0244a534ea96da0e049e74af00e
PGijsbers <p.gijsbers@tue.nl>
2018-08-25 21:35:47 -0700

    Adjustment for new ensemble. Predict_proba is now Classifier only.

--- gama/GamaClassifier.py
+++ gama/GamaClassifier.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 from .gama import Gama
 from gama.configuration.classification import clf_config
 from gama.ea.metrics import Metric
+from gama.utilities.auto_ensemble import EnsembleClassifier
 
 
 class GamaClassifier(Gama):
@@ -25,16 +26,24 @@ class GamaClassifier(Gama):
         super().__init__(*args, **kwargs, config=config, objectives=objectives)
 
     def predict(self, X):
-        if self._best_pipeline is not None:
-            return self._best_pipeline.predict(X)
-        raise NotImplementedError
-        # Have to rethink how we do Ensembles.
-        predictions = np.argmax(self.predict_proba(X), axis=1)
-        predictions = np.squeeze(predictions)
+        X = self._preprocess_predict_X(X)
+        class_predictions = self.ensemble.predict(X)
         if self._label_encoder:
-            return np.asarray(self._label_encoder.inverse_transform(predictions))
+            return np.asarray(self._label_encoder.inverse_transform(class_predictions))
         else:
-            return predictions
+            return class_predictions
+
+    def predict_proba(self, X):
+        """ Predict the class probabilities for input X.
+
+        Predict target for X, using the best found pipeline(s) during the `fit` call.
+
+        :param X: a 2d numpy array with the length of the second dimension is equal to that of X of `fit`.
+        :return: a numpy array with class probabilities. The array is of shape (N, K) where N is the length of the
+            first dimension of X, and K is the number of class labels found in `y` of `fit`.
+        """
+        X = self._preprocess_predict_X(X)
+        return self.ensemble.predict_proba(X)
 
     def fit(self, X, y, warm_start=False, auto_ensemble_n=25, restart=False, keep_cache=False):
         # Allow arbitrary class name (e.g. string or 1-indexed)
@@ -42,6 +51,10 @@ class GamaClassifier(Gama):
         y = self._label_encoder.transform(y)
 
         super().fit(X, y, warm_start, auto_ensemble_n, restart)
+
+    def _initialize_ensemble(self):
+        self.ensemble = EnsembleClassifier(self._scoring_function, self.y_train,
+                                           model_library_directory=self._cache_dir, n_jobs=self._n_jobs)
 
     def _build_fit_ensemble(self, ensemble_size, timeout):
         super()._build_fit_ensemble(ensemble_size, timeout)
