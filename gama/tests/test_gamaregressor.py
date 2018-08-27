a39c08bb2b6b994946f3444b92dd71039afd3458
PGijsbers <p.gijsbers@tue.nl>
2018-08-26 20:43:33 -0700

    Add test for missing values.

--- gama/tests/test_gamaregressor.py
+++ gama/tests/test_gamaregressor.py
@@ -54,7 +54,33 @@ class GamaRegressorSystemTestCase(unittest.TestCase):
                                 'predictions should be at least as good as predicting mean.')
 
     def test_regression_mean_squared_error(self):
+        """ GamaRegressor works on all-numeric data. """
         self._test_dataset_problem(boston, 'mean_squared_error')
+
+    def test_missing_value_regression(self):
+        """ GamaRegressor works when missing values are present. """
+        data = boston
+        metric = 'mean_squared_error'
+        X, y = data['load'](return_X_y=True)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+        X_train[1:300:2, 0] = X_train[2:300:5, 1] = float("NaN")
+        X_test[1:100:2, 0] = X_test[2:100:5, 1] = float("NaN")
+
+        gama = GamaRegressor(random_state=0, max_total_time=60, objectives=(metric, 'size'))
+        with Stopwatch() as sw:
+            gama.fit(X_train, y_train, auto_ensemble_n=5)
+
+        self.assertLessEqual(sw.elapsed_time, 60 * self._fit_time_margin, 'fit must stay within 110% of allotted time.')
+
+        predictions = gama.predict(X_test)
+        self.assertTrue(isinstance(predictions, np.ndarray), 'predictions should be numpy arrays.')
+        self.assertEqual(predictions.shape, (data['test_size'],), 'predict should return (N,) shaped array.')
+
+        # Majority classifier on this split achieves 0.6293706293706294
+        mse = mean_squared_error(y_test, predictions)
+        print(data['name'], metric, 'mse:', mse)
+        self.assertLessEqual(mse, data['base_mse'],
+                                'predictions should be at least as good as predicting mean.')
 
 
 if __name__ == '__main__':
