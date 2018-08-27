2100aa7a64a781c242910308f689f6c9bc4a33a8
PGijsbers <p.gijsbers@tue.nl>
2018-08-26 20:35:35 -0700

    New testcase: missing values. Added doc strings.

--- gama/tests/test_gamaclassifier.py
+++ gama/tests/test_gamaclassifier.py
@@ -79,22 +79,65 @@ class GamaClassifierSystemTestCase(unittest.TestCase):
                              'predictions should be at least as good as majority class.')
 
     def test_binary_classification_accuracy(self):
+        """ GamaClassifier can do binary classification with predit metric. """
         self._test_dataset_problem(breast_cancer, 'accuracy')
 
     def test_binary_classification_logloss(self):
+        """ GamaClassifier can do binary classification with predict-proba metric. """
         self._test_dataset_problem(breast_cancer, 'log_loss')
 
     def test_multiclass_classification_accuracy(self):
+        """ GamaClassifier can do multi-class with predict metric. """
         self._test_dataset_problem(wine, 'accuracy')
 
     def test_multiclass_classification_logloss(self):
+        """ GamaClassifier can do multi-class with predict-proba metric. """
         self._test_dataset_problem(wine, 'log_loss')
         
     def test_string_label_classification_accuracy(self):
+        """ GamaClassifier can work with string-like target labels when using predict-metric. """
         self._test_dataset_problem(breast_cancer, 'accuracy', labelled_y=True)
 
     def test_string_label_classification_log_loss(self):
+        """ GamaClassifier can work with string-type target labels when using predict-proba metric. """
         self._test_dataset_problem(breast_cancer, 'log_loss', labelled_y=True)
+
+    def test_missing_value_classification(self):
+        """ GamaClassifier handles missing data. """
+        data = breast_cancer
+        metric = 'log_loss'
+
+        X, y = data['load'](return_X_y=True)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
+        X_train[1:300:2, 0] = X_train[2:300:5, 1] = float("NaN")
+        X_test[1:100:2, 0] = X_test[2:100:5, 1] = float("NaN")
+
+        gama = GamaClassifier(random_state=0, max_total_time=60, objectives=(metric, 'size'))
+        with Stopwatch() as sw:
+            gama.fit(X_train, y_train, auto_ensemble_n=5)
+
+        self.assertLessEqual(sw.elapsed_time, 60 * self._fit_time_margin, 'fit must stay within 110% of allotted time.')
+
+        class_predictions = gama.predict(X_test)
+        self.assertTrue(isinstance(class_predictions, np.ndarray), 'predictions should be numpy arrays.')
+        self.assertEqual(class_predictions.shape, (data['test_size'],), 'predict should return (N,) shaped array.')
+
+        # Majority classifier on this split achieves 0.6293706293706294
+        accuracy = accuracy_score(y_test, class_predictions)
+        print(data['name'], metric, 'accuracy:', accuracy)
+        self.assertGreaterEqual(accuracy, data['base_accuracy'],
+                                'predictions should be at least as good as majority class.')
+
+        class_probabilities = gama.predict_proba(X_test)
+        self.assertTrue(isinstance(class_probabilities, np.ndarray), 'probability predictions should be numpy arrays.')
+        self.assertEqual(class_probabilities.shape, (data['test_size'], data['n_classes']),
+                         'predict_proba should return (N,K) shaped array.')
+
+        # Majority classifier on this split achieves 12.80138131184662
+        logloss = log_loss(y_test, class_probabilities)
+        print(data['name'], metric, 'log-loss:', logloss)
+        self.assertLessEqual(logloss, data['base_log_loss'],
+                             'predictions should be at least as good as majority class.')
 
 
 if __name__ == '__main__':
