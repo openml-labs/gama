6cc0c19003d92207de9143f08f7482dbe97c2f40
PGijsbers <p.gijsbers@tue.nl>
2018-09-03 06:29:35 -0700

    Define label encoding, remove custom fit.

--- gama/GamaClassifier.py
+++ gama/GamaClassifier.py
@@ -41,12 +41,9 @@ class GamaClassifier(Gama):
         X = self._preprocess_predict_X(X)
         return self.ensemble.predict_proba(X)
 
-    def fit(self, X, y, skip_preprocess=False, warm_start=False, auto_ensemble_n=25, restart=False, keep_cache=False):
-        # Allow arbitrary class name (e.g. string or 1-indexed)
+    def _encode_labels(self, y):
         self._label_encoder = LabelEncoder().fit(y)
-        y = self._label_encoder.transform(y)
-
-        super().fit(X, y, skip_preprocess, warm_start, auto_ensemble_n, restart)
+        return self._label_encoder.transform(y)
 
     def _initialize_ensemble(self):
         self.ensemble = EnsembleClassifier(self._scoring_function, self.y_train,
