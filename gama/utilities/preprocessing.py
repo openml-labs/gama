4d54df24bc95e7d7454bf37f041215fbffa16e2c
PGijsbers <p.gijsbers@tue.nl>
2018-09-05 08:13:18 -0700

    Added docstring.

--- gama/utilities/preprocessing.py
+++ gama/utilities/preprocessing.py
@@ -3,14 +3,22 @@ import pandas as pd
 from sklearn.preprocessing import Imputer
 
 
-def define_preprocessing_steps(df, max_extra_features_created=None, max_categories_for_one_hot=10):
+def define_preprocessing_steps(X_df, max_extra_features_created=None, max_categories_for_one_hot=10):
+    """ Constructs the preprocessing steps for the dataframe, taking into account data types.
+
+    :param X_df: Features of the data (i.e. without target label).
+    :param max_extra_features_created: If set, dynamically decrease max_categories_for_one_hot as needed so as not to
+        create a greater amount of features than max_extra_features_created.
+    :param max_categories_for_one_hot: The maximum amount of unique category levels to be considered for one hot encoding.
+    :return: a list of preprocessing step objects.
+    """
     if max_extra_features_created:
         # Will determine max_categories_for_one_hot based on how many total new features would be created.
         raise NotImplementedError()
 
     one_hot_columns = []
     target_encoding_columns = []
-    for unique_values, dtype, column_index in zip(df.apply(pd.Series.nunique), df.dtypes, df.columns):
+    for unique_values, dtype, column_index in zip(X_df.apply(pd.Series.nunique), X_df.dtypes, X_df.columns):
         if isinstance(dtype, pd.core.dtypes.dtypes.CategoricalDtype):
             if unique_values > max_categories_for_one_hot:
                 target_encoding_columns.append(column_index)
