f9ab161b1cde6a13d875d6a49cbcb602f4e40736
PGijsbers <p.gijsbers@tue.nl>
2018-09-25 15:40:43 +0200

    Add test for missing values in ARFF files.

--- gama/tests/test_gamaclassifier.py
+++ gama/tests/test_gamaclassifier.py
@@ -12,13 +12,22 @@ from gama import GamaClassifier
 
 
 def gamaclassifier_test_suite():
-    test_cases = [GamaClassifierARFFSystemTestCase]#, GamaClassifierSystemTestCase]
+    test_cases = [GamaClassifierARFFSystemTestCase, GamaClassifierSystemTestCase]
     return unittest.TestSuite(map(unittest.TestLoader().loadTestsFromTestCase, test_cases))
 
 
 # While we could derive statistics dynamically, we want to know if any changes ever happen, so we save them statically.
 breast_cancer = dict(
     name='breast_cancer',
+    load=load_breast_cancer,
+    test_size=143,
+    n_classes=2,
+    base_accuracy=0.62937,
+    base_log_loss=12.80138
+)
+
+breast_cancer_missing = dict(
+    name='breast_cancer_missing',
     load=load_breast_cancer,
     test_size=143,
     n_classes=2,
@@ -171,14 +180,11 @@ class GamaClassifierARFFSystemTestCase(unittest.TestCase):
         pass
 
     def _test_dataset_problem(self, data, metric):
-        train_path = 'data/{}_train.arff'.format(data['name'])
-        test_path = 'data/{}_test.arff'.format(data['name'])
+        train_path = 'gama/tests/data/{}_train.arff'.format(data['name'])
+        test_path = 'gama/tests/data/{}_test.arff'.format(data['name'])
         gama = GamaClassifier(random_state=0, max_total_time=60, objectives=(metric, 'size'))
 
         X, y = data['load'](return_X_y=True)
-        #if labelled_y:
-        #    databunch = data['load']()
-        #    y = [databunch.target_names[c_i] for c_i in databunch.target]
         X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
         y_test = [str(val) for val in y_test]
 
@@ -198,7 +204,6 @@ class GamaClassifierARFFSystemTestCase(unittest.TestCase):
 
         class_probabilities = gama.predict_proba(arff_file_path=test_path)
 
-        print(list(zip(class_predictions, y_test, class_probabilities)))
         self.assertTrue(isinstance(class_probabilities, np.ndarray), 'probability predictions should be numpy arrays.')
         self.assertEqual(class_probabilities.shape, (data['test_size'], data['n_classes']),
                          'predict_proba should return (N,K) shaped array.')
@@ -224,42 +229,9 @@ class GamaClassifierARFFSystemTestCase(unittest.TestCase):
         """ GamaClassifier can do multi-class with predict-proba metric. """
         self._test_dataset_problem(wine, 'log_loss')
 
-    # def test_missing_value_classification(self):
-    #     """ GamaClassifier handles missing data. """
-    #     data = breast_cancer
-    #     metric = 'log_loss'
-    #
-    #     X, y = data['load'](return_X_y=True)
-    #     X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
-    #     X_train[1:300:2, 0] = X_train[2:300:5, 1] = float("NaN")
-    #     X_test[1:100:2, 0] = X_test[2:100:5, 1] = float("NaN")
-    #
-    #     gama = GamaClassifier(random_state=0, max_total_time=60, objectives=(metric, 'size'))
-    #     with Stopwatch() as sw:
-    #         gama.fit(X_train, y_train, auto_ensemble_n=5)
-    #
-    #     self.assertLessEqual(sw.elapsed_time, 60 * self._fit_time_margin, 'fit must stay within 110% of allotted time.')
-    #
-    #     class_predictions = gama.predict(X_test)
-    #     self.assertTrue(isinstance(class_predictions, np.ndarray), 'predictions should be numpy arrays.')
-    #     self.assertEqual(class_predictions.shape, (data['test_size'],), 'predict should return (N,) shaped array.')
-    #
-    #     # Majority classifier on this split achieves 0.6293706293706294
-    #     accuracy = accuracy_score(y_test, class_predictions)
-    #     print(data['name'], metric, 'accuracy:', accuracy)
-    #     self.assertGreaterEqual(accuracy, data['base_accuracy'],
-    #                             'predictions should be at least as good as majority class.')
-    #
-    #     class_probabilities = gama.predict_proba(X_test)
-    #     self.assertTrue(isinstance(class_probabilities, np.ndarray), 'probability predictions should be numpy arrays.')
-    #     self.assertEqual(class_probabilities.shape, (data['test_size'], data['n_classes']),
-    #                      'predict_proba should return (N,K) shaped array.')
-    #
-    #     # Majority classifier on this split achieves 12.80138131184662
-    #     logloss = log_loss(y_test, class_probabilities)
-    #     print(data['name'], metric, 'log-loss:', logloss)
-    #     self.assertLessEqual(logloss, data['base_log_loss'],
-    #                          'predictions should be at least as good as majority class.')
+    def test_missing_value_classification(self):
+        """ GamaClassifier handles missing data. """
+        self._test_dataset_problem(breast_cancer_missing, 'log_loss')
 
 
 if __name__ == '__main__':
