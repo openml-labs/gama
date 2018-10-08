232ed0038c4eb40fddf9c9c84746e1f7b67d9a55
PGijsbers <p.gijsbers@tue.nl>
2018-10-08 14:05:03 +0200

    Add mutation/crossover. Start on Events.

--- gama/utilities/logging_utilities.py
+++ gama/utilities/logging_utilities.py
@@ -48,13 +48,41 @@ class TOKENS:
     EA_RESTART = 'EA_RST'
     EA_REMOVE_IND = 'RMV_IND'
     EVALUATION_TIMEOUT = 'EVAL_TO'
+    MUTATION = 'IND_MUT'
+    CROSSOVER = "IND_CX"
+
+
+def default_time_format(datetime_):
+    return datetime_.strftime(TIME_FORMAT)#[:-3]
 
 
 def log_parseable_event(log_, token, *args):
+    args = [default_time_format(arg) if isinstance(arg, datetime) else arg for arg in args]
     start = "PLE;{};".format(token)
     attrs = ';'.join([str(arg) for arg in args])
-    end = ';' + datetime.now().strftime(TIME_FORMAT)[:-3] + ';END!'
+    end = ';' + default_time_format(datetime.now()) + ';END!'
     log_.debug(start+attrs+end)
 
 
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S,%f'
+
+
+class Event:
+
+    def __init__(self, start, end, token):
+        self.start_time = start
+        self.end_time = end
+        self.token = token
+
+    @property
+    def duration(self):
+        return (self.end_time - self.start_time).total_seconds()
+
+    @classmethod
+    def from_string(cls, string):
+        token, *args = string.split('PLE;')[1].split(';END!')[0].split(';')
+        if 'EVAL' in token:
+            start, end, pl, *args = args
+
+        datetime.strptime(start, TIME_FORMAT)
+        return cls(token, start, end)
