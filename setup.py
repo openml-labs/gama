0507f1230fe2ee39ebfbc74c003f9a0343e3f262
PGijsbers <p.gijsbers@tue.nl>
2018-11-19 17:22:24 +0100

    Updated requirements.

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ requirements = [
     'numpy>=1.14.0',
     'scipy>=1.0.0',
     'scikit-learn==0.20.0',
-    'deap>=1.2.2',
     'stopit>=1.1.1',
     'liac-arff>=2.2.2',
     'category-encoders>=1.2.8'
