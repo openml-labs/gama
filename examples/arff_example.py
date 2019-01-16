60cfb25787224c7119e9f0b0c4ff5573e437d9ae
PGijsbers <p.gijsbers@tue.nl>
2019-01-16 18:39:27 +0100

    [skip ci] Don't store analysis log for examples.

--- examples/arff_example.py
+++ examples/arff_example.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     print("Make sure you adjust the file path if not executed from the examples directory.")
     file_path = "../tests/data/breast_cancer_{}.arff"
 
-    automl = GamaClassifier(max_total_time=180)
+    automl = GamaClassifier(max_total_time=180, keep_analysis_log=False)
     print("Starting `fit` which will take roughly 3 minutes.")
     automl.fit(arff_file_path=file_path.format('train'))
 
