2e698a3180395a106627a4fe38b3928a41558e4b
Gijsbers <p.gijsbers@tue.nl>
2019-06-03 15:57:37 +0200

    Cancel futures before termination.

--- gama/genetic_programming/algorithms/async_ea.py
+++ gama/genetic_programming/algorithms/async_ea.py
@@ -89,9 +89,9 @@ def async_ea(start_population, toolbox, evaluation_callback=None, restart_callba
                         new_individual = toolbox.create(current_population, 1)[0]
                         futures.add(async.schedule(evaluate_log, (new_individual,)))
 
+    for future in futures:
+        future.cancel()
     async.stop()
-    #for future in futures:
-    #    future.cancel()
 
     if not c_mgr:
         log.info('Asynchronous EA terminated because maximum time has elapsed.'
