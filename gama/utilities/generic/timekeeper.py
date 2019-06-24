460dc37aba041743fa50c998ff796c57f5d4a273
Gijsbers <p.gijsbers@tue.nl>
2019-06-24 10:58:11 +0200

    Save all activity related information in a Tuple.

--- gama/utilities/generic/timekeeper.py
+++ gama/utilities/generic/timekeeper.py
@@ -1,42 +1,58 @@
 from contextlib import contextmanager
-from .stopwatch import Stopwatch
+from typing import Iterator, Optional, NamedTuple
 import logging
-from typing import Iterator
 
+from .stopwatch import Stopwatch
 
 log = logging.getLogger(__name__)
+
+
+class Activity(NamedTuple):
+    name: str
+    time_limit: Optional[int]
+    stopwatch: Stopwatch
 
 
 class TimeKeeper:
     """ Simple object that helps keep track of time over multiple activities. """
 
     def __init__(self, total_time: int=0):
-        self.activity_durations = {}
         self.total_time = total_time
-        self._current_activity_stopwatch = None
+        self.current_activity = None
+        self.activities = []
 
     @property
     def total_time_remaining(self) -> float:
         """ Return time remaining in seconds. """
         if self.total_time > 0:
-            return self.total_time - sum(self.activity_durations.values())
+            return self.total_time - sum(map(lambda a: a.stopwatch.elapsed_time, self.activities))
         raise RuntimeError("Time Remaining only available if `total_time` was set on init.")
 
     @property
     def current_activity_time_elapsed(self) -> float:
         """ Return elapsed time in seconds of current activity. Raise RuntimeError if no current activity. """
-        if self._current_activity_stopwatch is not None:
-            return self._current_activity_stopwatch.elapsed_time
+        if self.current_activity is not None:
+            return self.current_activity.stopwatch.elapsed_time
         else:
             raise RuntimeError("No activity in progress.")
 
+    @property
+    def current_activity_time_left(self) -> float:
+        """ Return time left in seconds for current activity if a time limit was indicated. """
+        if self.current_activity is not None and self.current_activity.time_limit is not None:
+            return self.current_activity.time_limit - self.current_activity.stopwatch.elapsed_time
+        elif self.current_activity is None:
+            raise RuntimeError("No activity in progress.")
+        else:
+            raise RuntimeError("No time limit specified for activity {}.")
+
     @contextmanager
-    def start_activity(self, activity: str) -> Iterator[Stopwatch]:
+    def start_activity(self, activity: str, time_limit: Optional[int] = None) -> Iterator[Stopwatch]:
         """ Mark the start of a new activity and automatically time its duration.
             TimeManager does not currently support nested activities. """
         with Stopwatch() as sw:
-            self._current_activity_stopwatch = sw
+            self.current_activity = Activity(activity, time_limit, sw)
+            self.activities.append(self.current_activity)
             yield sw
-        self.activity_durations[activity] = sw.elapsed_time
-        self._current_activity_stopwatch = None
+        self.current_activity = None
         log.info("{} took {:.4f}s.".format(activity, sw.elapsed_time))
