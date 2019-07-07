d807d8f60b6cd10bd31c884f1d13e7920facd97a
Gijsbers <p.gijsbers@tue.nl>
2019-07-07 14:05:10 -0700

    Specify test dependencies in test through extras_require

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 requirements = [
     'numpy>=1.14.0',
     'scipy>=1.0.0',
-    'scikit-learn==0.20.0',
+    'scikit-learn>=0.20.0',
     'stopit>=1.1.1',
     'liac-arff>=2.2.2',
     'category-encoders>=1.2.8'
@@ -28,6 +28,6 @@ setup(
     url='https://github.com/PGijsbers/GAMA',
     packages=find_packages(exclude=['tests']),
     install_requires=requirements,
-    tests_require=test_requirements,
+    extras_require={'test': test_requirements},
     python_requires='>=3.5.0'
 )
