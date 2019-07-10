7981e9d60ca1bb8165c4d93edce3df0b5ece7381
Gijsbers <p.gijsbers@tue.nl>
2019-07-10 12:25:37 -0700

    Created preprocessing own test module.

--- tests/unit/test_gama.py
+++ tests/unit/test_gama.py
@@ -1,14 +1,4 @@
-import itertools
-import pandas as pd
-import pytest
-
 import gama
-
-@pytest.fixture
-def gamaclassifier():
-    gc = gama.GamaClassifier()
-    gc.delete_cache()
-    yield gc
 
 
 def test_reproducible_initialization():
@@ -19,19 +9,3 @@ def test_reproducible_initialization():
     pop2 = [g2._operator_set.individual() for _ in range(10)]
     for ind1, ind2 in zip(pop1, pop2):
         assert ind1.pipeline_str() == ind2.pipeline_str(), "The initial population should be reproducible."
-
-
-def test_format_x_y(gamaclassifier):
-    """ Tests that X and y data correctly get converted to (pd.DataFrame, pd.DataFrame). """
-    def well_formatted_x_y(x, y):
-        assert isinstance(x, pd.DataFrame)
-        assert isinstance(y, pd.DataFrame)
-        assert len(x) == len(y)
-
-    from sklearn.datasets import load_digits
-    X_np, y_np = load_digits(return_X_y=True)
-    X_df, y_df = pd.DataFrame(X_np), pd.DataFrame(y_np)
-    y_series = pd.Series(y_np)
-
-    for X, y in itertools.product([X_np, X_df], [y_np, y_series, y_df]):
-        well_formatted_x_y(*gamaclassifier._format_x_y(X, y))
