4c8d352b3817754346a706f6ec10acd58632fb96
PGijsbers <p.gijsbers@tue.nl>
2019-08-09 11:15:50 +0200

    sketch of multi-log visualization

--- gama/visualization/apps/load_file_page.py
+++ gama/visualization/apps/load_file_page.py
@@ -6,6 +6,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from gama.visualization.app import app
+from gama.visualization.apps.aggregate_report_page import aggregate_report_page
 from gama.visualization.apps.single_report_page import single_report_page
 
 
@@ -27,8 +28,7 @@ load_file_page = html.Div([
             'textAlign': 'center',
             'margin': '10px'
         },
-        # For now we only visualize single traces:
-        multiple=False
+        multiple=True
     ),
     html.Div(id='load-error-message')
 ])
@@ -40,8 +40,16 @@ load_file_page = html.Div([
                State('upload-data', 'last_modified')])
 def update_output(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
-        content_type, content_string = list_of_contents.split(',')
-        decoded = base64.b64decode(content_string).decode('utf-8')
-        log_lines = decoded.splitlines()
-        return single_report_page(log_lines, list_of_names)
+        if len(list_of_contents) == 1:
+            content_type, content_string = list_of_contents[0].split(',')
+            decoded = base64.b64decode(content_string).decode('utf-8')
+            log_lines = decoded.splitlines()
+            return single_report_page(log_lines, list_of_names)
+        else:
+            logs = []
+            for loc in list_of_contents:
+                content_type, content_string = loc.split(',')
+                decoded = base64.b64decode(content_string).decode('utf-8')
+                logs.append(decoded.splitlines())
+            return aggregate_report_page(logs)
     return load_file_page
