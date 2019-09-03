ef8f5259eb5c4faf6a8bfaee64645631b16878be
PGijsbers <p.gijsbers@tue.nl>
2019-09-03 10:53:00 +0200

    :fire: Remove Event as gama events are not yet a thing.

--- gama/logging/machine_logging.py
+++ gama/logging/machine_logging.py
@@ -44,24 +44,3 @@ def log_event(log_: object, token: object, *args: object) -> object:
     attrs = f'{PLE_DELIM}'.join([str(arg) for arg in args])
     message = f'{PLE_DELIM}'.join([PLE_START, token, attrs, default_time_format(datetime.now()), PLE_END])
     log_.log(level=MACHINE_LOG_LEVEL, msg=message)
-
-
-class Event:
-
-    def __init__(self, start, end, token):
-        self.start_time = start
-        self.end_time = end
-        self.token = token
-
-    @property
-    def duration(self):
-        return (self.end_time - self.start_time).total_seconds()
-
-    @classmethod
-    def from_string(cls, string):
-        token, *args = string.split('PLE;')[1].split(';END!')[0].split(';')
-        if 'EVAL' in token:
-            start, end, pl, *args = args
-
-        datetime.strptime(start, TIME_FORMAT)
-        return cls(token, start, end)
