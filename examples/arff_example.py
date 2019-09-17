dec6b5ab7b7cbb8bc8ee05f17063327a75331b6d
PGijsbers <p.gijsbers@tue.nl>
2019-09-17 15:33:00 +0200

    :white_check_mark: Update examples.

--- examples/arff_example.py
+++ examples/arff_example.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     print("Make sure you adjust the file path if not executed from the examples directory.")
     file_path = "../tests/data/breast_cancer_{}.arff"
 
-    automl = GamaClassifier(max_total_time=180, keep_analysis_log=False, n_jobs=1)
+    automl = GamaClassifier(max_total_time=180, keep_analysis_log=None, n_jobs=1)
     print("Starting `fit` which will take roughly 3 minutes.")
     automl.fit_arff(file_path.format('train'))
 
