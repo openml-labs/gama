d561689ff96cdc3145b82d7e953dc9c1f560d645
PGijsbers <p.gijsbers@tue.nl>
2019-11-10 17:06:17 +0100

    :bug: Fix an issue where unlabeled samples would not be removed before training.

--- gama/utilities/preprocessing.py
+++ gama/utilities/preprocessing.py
@@ -106,8 +106,8 @@ def format_x_y(x: Union[pd.DataFrame, np.ndarray], y: Union[pd.DataFrame, pd.Ser
 
     if remove_unlabeled:
         unlabeled = y[y.columns[0]].isnull() if isinstance(y, pd.DataFrame) else y.isnull()
-        if unlabeled.isnull().any():
+        if unlabeled.any():
             log.info("Target vector has been found to contain NaN-labels, these rows will be ignored.")
-            x, y = x.loc[~y.isnull(), :], y[~y.isnull(), :]
+            x, y = x.loc[~unlabeled], y.loc[~unlabeled]
 
     return x, y
