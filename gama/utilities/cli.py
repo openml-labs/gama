bdec4911916d616d49275cb711389b45b8195f92
PGijsbers <p.gijsbers@tue.nl>
2019-11-10 17:03:00 +0100

    :construction: Working data type interpretation.

--- gama/utilities/cli.py
+++ gama/utilities/cli.py
@@ -2,7 +2,8 @@ import argparse
 import logging
 import pandas as pd
 
-from gama import GamaClassifier
+from gama import GamaClassifier, GamaRegressor
+from gama.data import load_feature_metadata_from_arff
 
 
 def parse_args():
@@ -26,11 +27,11 @@ def parse_args():
     #     help="For CSV files only, the character used to separate columns in the input file. (default=',')"
     # )
     io_group.add_argument(
-        '-target',
+        '--target',
         dest='target',
         type=str,
         default=None,
-        help="The target column in the input file"
+        help="The target column in the input file."
     )
     io_group.add_argument(
         '-o',
@@ -41,6 +42,14 @@ def parse_args():
     )
 
     optimization = parser.add_argument_group("Optimization")
+    optimization.add_argument(
+        '--mode',
+        dest='mode',
+        type=str,
+        default=None,
+        help="By default, it is determined automatically from the input file."
+             "Use this parameter force 'classification' or 'regression' mode. "
+    )
     optimization.add_argument(
         '-m',
         dest='metric',
@@ -93,12 +102,24 @@ def parse_args():
 
 def main():
     args = parse_args()
+
+    print('CLI: Processing input')
     if args.input_file.lower().endswith('.csv'):
         raise NotImplementedError("CSV currently not supported.")
         # data = pd.read_csv(args.input_file, sep=args.separator)
+    if args.input_file.lower().endswith('.arff') and args.mode is None:
+        attributes = load_feature_metadata_from_arff(args.input_file)
+        target = list(attributes)[-1] if args.target is None else args.target
+        target_type = attributes[target]
+        if '{' in target_type:
+            # Nominal features are denoted by listen all their values, eg. {VALUE_1, VALUE_2, ...}
+            args.mode = 'classification'
+        elif target_type.lower() == 'real':
+            args.mode = 'regression'
+        else:
+            raise ValueError(f"Target column {target} has type {target_type}, which GAMA can't model.")
 
-    # TODO: Determine problem type. -> Assume classification for now
-
+    print('CLI: Initializing GAMA')
     log_level = logging.INFO if args.verbose else logging.WARNING
     configuration = dict(
         regularize_length=args.prefer_short,
@@ -110,14 +131,21 @@ def main():
     if args.metric:
         configuration['scoring'] = args.metric
 
-    automl = GamaClassifier(**configuration)
+    if args.mode == 'regression':
+        automl = GamaRegressor(**configuration)
+    elif args.mode == 'classification':
+        automl = GamaClassifier(**configuration)
+    else:
+        raise ValueError(f"Mode {args.mode} is not valid (--mode).")
+
+    print('CLI: Starting model search')
     if args.input_file.lower().endswith('.arff'):
         automl.fit_arff(args.input_file.lower(), target_column=args.target)
     #else:
     #    automl.fit(x, y)
 
-    # Save model/export script
-    print('Not saving anything.')
+    # == Model Export ===
+    print('CLI: Exporting models.')
     print('done!')
 
 
