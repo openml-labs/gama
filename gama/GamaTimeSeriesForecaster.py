693e557ef1f7f693ec58697c703d20c3d8be4b4d
Dayne Freitag <daynefreitag@sri.com>
2020-02-06 09:12:05 -0800

    Updates to get time series forecasting to work in the D3M context

--- gama/GamaTimeSeriesForecaster.py
+++ gama/GamaTimeSeriesForecaster.py
@@ -5,15 +5,40 @@ from functools import partial
 from .GamaRegressor import GamaRegressor
 from .genetic_programming.compilers.scikitlearn import evaluate_individual, cross_val_predict_timeseries
 
+GKEY_TYPE = "https://metadata.datadrivendiscovery.org/types/GroupingKey"
+SGKEY_TYPE = "https://metadata.datadrivendiscovery.org/types/SuggestedGroupingKey"
 
-def timeseries_cv(X, numfolds):
-    n = X.shape[0]
+def timeseries_cv(indices, numfolds):
+    n = len(indices)
     foldsize = n / (numfolds + 1)
     for i in range(numfolds):
-        train = np.arange(0, int(foldsize * (i+1)), dtype=int)
-        test = np.arange(int(foldsize * (i+1)), int(foldsize * (i+2)), dtype=int)
+        train = np.asarray(indices[0:int(foldsize * (i+1))], dtype=int)
+        test = np.asarray(indices[int(foldsize * (i+1)):int(foldsize * (i+2))], dtype=int)
+        # train = np.arange(0, int(foldsize * (i+1)), dtype=int)
+        # test = np.arange(int(foldsize * (i+1)), int(foldsize * (i+2)), dtype=int)
         yield train, test
 
+
+def time_series_groups(X):
+    grouping_keys = X.metadata.get_columns_with_semantic_type(GKEY_TYPE)
+    suggested_grouping_keys = X.metadata.get_columns_with_semantic_type(SGKEY_TYPE)
+    if len(grouping_keys) == 0:
+        grouping_keys = suggested_grouping_keys
+    # No grouping keys.  All the data represents one time series.
+    if len(grouping_keys) == 0:
+        yield list(X.index)
+        return
+    assert len(grouping_keys) == 1
+    colname = X.columns[grouping_keys[0]]
+    for val, df in X.groupby(colname):
+        yield list(df.index)
+
+
+def timeseries_cv_grouped(X, numfolds):
+    for group in time_series_groups(X):
+        for train, test in timeseries_cv(group, numfolds):
+            yield train, test
+        break
 
 class GamaTimeSeriesForecaster(GamaRegressor):
     """ Wrapper for the toolbox logic executing the AutoML pipeline for time
@@ -24,7 +49,7 @@ class GamaTimeSeriesForecaster(GamaRegressor):
         deadline = time.time() + timeout
         evaluate_args = dict(evaluate_pipeline_length=self._regularize_length, X=self._X, y_train=self._y,
                              metrics=self._metrics, cache_dir=self._cache_dir, timeout=self._max_eval_time,
-                             deadline=deadline, cv=[split for split in timeseries_cv(self._X, 5)],
+                             deadline=deadline, cv=[split for split in timeseries_cv_grouped(self._X, 5)],
                              cvpredict=cross_val_predict_timeseries)
         self._operator_set.evaluate = partial(evaluate_individual, **evaluate_args)
 
--- gama/genetic_programming/compilers/scikitlearn.py
+++ gama/genetic_programming/compilers/scikitlearn.py
@@ -84,108 +84,7 @@ def cross_val_predict_score(estimator, X, y_train, metrics=None, cvpredict=cross
 
 
 def cross_val_predict_timeseries(estimator, X, y=None, groups=None, cv='warn',
-                                 n_jobs=None, verbose=0, fit_params=None,
-                                 pre_dispatch='2*n_jobs', method='predict'):
-    """Generate cross-validated estimates for each input data point
-
-    It is not appropriate to pass these predictions into an evaluation
-    metric. Use :func:`cross_validate` to measure generalization error.
-
-    Read more in the :ref:`User Guide <cross_validation>`.
-
-    Parameters
-    ----------
-    estimator : estimator object implementing 'fit' and 'predict'
-        The object to use to fit the data.
-
-    X : array-like
-        The data to fit. Can be, for example a list, or an array at least 2d.
-
-    y : array-like, optional, default: None
-        The target variable to try to predict in the case of
-        supervised learning.
-
-    groups : array-like, with shape (n_samples,), optional
-        Group labels for the samples used while splitting the dataset into
-        train/test set.
-
-    cv : int, cross-validation generator or an iterable, optional
-        Determines the cross-validation splitting strategy.
-        Possible inputs for cv are:
-
-        - None, to use the default 3-fold cross validation,
-        - integer, to specify the number of folds in a `(Stratified)KFold`,
-        - :term:`CV splitter`,
-        - An iterable yielding (train, test) splits as arrays of indices.
-
-        For integer/None inputs, if the estimator is a classifier and ``y`` is
-        either binary or multiclass, :class:`StratifiedKFold` is used. In all
-        other cases, :class:`KFold` is used.
-
-        Refer :ref:`User Guide <cross_validation>` for the various
-        cross-validation strategies that can be used here.
-
-        .. versionchanged:: 0.20
-            ``cv`` default value if None will change from 3-fold to 5-fold
-            in v0.22.
-
-    n_jobs : int or None, optional (default=None)
-        The number of CPUs to use to do the computation.
-        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
-        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
-        for more details.
-
-    verbose : integer, optional
-        The verbosity level.
-
-    fit_params : dict, optional
-        Parameters to pass to the fit method of the estimator.
-
-    pre_dispatch : int, or string, optional
-        Controls the number of jobs that get dispatched during parallel
-        execution. Reducing this number can be useful to avoid an
-        explosion of memory consumption when more jobs get dispatched
-        than CPUs can process. This parameter can be:
-
-            - None, in which case all the jobs are immediately
-              created and spawned. Use this for lightweight and
-              fast-running jobs, to avoid delays due to on-demand
-              spawning of the jobs
-
-            - An int, giving the exact number of total jobs that are
-              spawned
-
-            - A string, giving an expression as a function of n_jobs,
-              as in '2*n_jobs'
-
-    method : string, optional, default: 'predict'
-        Invokes the passed method name of the passed estimator. For
-        method='predict_proba', the columns correspond to the classes
-        in sorted order.
-
-    Returns
-    -------
-    predictions : ndarray
-        This is the result of calling ``method``
-    y
-        The subset of y tested against
-
-    See also
-    --------
-    cross_val_score : calculate score for each CV split
-
-    cross_validate : calculate one or more scores and timings for each CV split
-
-    Notes
-    -----
-    In the case that one or more classes are absent in a training portion, a
-    default score needs to be assigned to all instances for that class if
-    ``method`` produces columns per class, as in {'decision_function',
-    'predict_proba', 'predict_log_proba'}.  For ``predict_proba`` this value is
-    0.  In order to ensure finite output, we approximate negative infinity by
-    the minimum finite float value for the dtype in other cases.
-
-    """
+                                 verbose=0, fit_params=None, method='predict'):
     X, y, groups = indexable(X, y, groups)
     cv = check_cv(cv, y, classifier=is_classifier(estimator))
 
@@ -193,27 +92,15 @@ def cross_val_predict_timeseries(estimator, X, y=None, groups=None, cv='warn',
         le = LabelEncoder()
         y = le.fit_transform(y)
 
-    # We clone the estimator to make sure that all the folds are
-    # independent, and that it is pickle-able.
-    parallel = Parallel(n_jobs=n_jobs, verbose=verbose,
-                        pre_dispatch=pre_dispatch)
     prediction_blocks = []
     y_part = []
     for train, test in cv.split(X, y, groups):
         preds = _fit_and_predict(clone(estimator), X, y, train, test, verbose, fit_params, method)
         prediction_blocks.append(preds)
         y_part.append(test)
-#    prediction_blocks = parallel(delayed(_fit_and_predict)(
-#            clone(estimator), X, y, train, test, verbose, fit_params, method)
-#        for train, test in cv.split(X, y, groups))
 
     # Concatenate the predictions
     predictions = [pred_block_i for pred_block_i, _ in prediction_blocks]
-#    test_indices = np.concatenate([indices_i
-#                                   for _, indices_i in prediction_blocks])
-
-    #inv_test_indices = np.empty(len(test_indices), dtype=int)
-    #inv_test_indices[test_indices] = np.arange(len(test_indices))
 
     # Check for sparse predictions
     if sp.issparse(predictions[0]):
@@ -221,7 +108,7 @@ def cross_val_predict_timeseries(estimator, X, y=None, groups=None, cv='warn',
     else:
         predictions = np.concatenate(predictions)
     y_train = np.concatenate(y_part)
-    return predictions, y.iloc[y_train,:]
+    return predictions, y.iloc[y_train, :]
 
 
 def object_is_valid_pipeline(o):
