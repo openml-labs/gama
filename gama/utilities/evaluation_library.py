d394a33a7a2d615be16fd9a41430e3f321d587c3
PGijsbers <p.gijsbers@tue.nl>
2020-02-11 11:49:45 +0100

    :construction::white_check_mark: Save evaluations outside of top m, but discard estimators and predictions.

--- gama/utilities/evaluation_library.py
+++ gama/utilities/evaluation_library.py
@@ -2,6 +2,7 @@ import heapq
 from typing import Tuple, List, Optional, Iterable, Union
 
 import numpy as np
+import pandas as pd
 
 from gama.genetic_programming.components import Individual
 
@@ -10,14 +11,26 @@ class Evaluation:
     """ Record relevant evaluation data of an individual. """
     __slots__ = ['individual', 'score', 'predictions', 'estimators', 'start_time', 'duration', 'error']
 
-    def __init__(self, individual: Individual):
+    def __init__(
+            self,
+            individual: Individual,
+            predictions: Optional[Union[np.ndarray, pd.Series, pd.DataFrame]] = None,
+            score: Tuple[float, ...] = None,
+            estimators: Optional[List] = None,
+            start_time: float = -1,
+            duration: float = -1,
+            error: str = None
+    ):
         self.individual: Individual = individual
-        self.score: Tuple[float, ...] = ()
-        self.predictions: Optional[np.ndarray] = None
-        self.estimators: List[object] = []
-        self.start_time: float = -1
-        self.duration: float = -1
-        self.error = None
+        self.score = () if score is None else score
+        self.estimators = [] if estimators is None else estimators
+        self.start_time = start_time
+        self.duration = duration
+        self.error = error
+
+        if isinstance(predictions, (pd.Series, pd.DataFrame)):
+            predictions = predictions.values
+        self.predictions: Optional[np.ndarray] = predictions
 
     # Is there a better way to do this? Assignment in __init__ is not preferred even if it saves lines.
     def __lt__(self, other): return self.score.__lt__(other.score)
@@ -29,19 +42,21 @@ class Evaluation:
 
 
 class EvaluationLibrary:
-    """ Maintains an in-memory record of the top n evaluations. """
+    """ Maintains an in-memory record of evaluations. """
 
     def __init__(
             self,
-            max_number_of_evaluations: Optional[int] = 200,
+            m: Optional[int] = 200,
             prediction_sample: Optional[Union[int, np.ndarray]] = None
     ):
         """
         
         Parameters
         ----------
-        max_number_of_evaluations: int, optional (default=200)
-            Maximum number of evaluations to keep in memory with predictions and fitted pipelines.
+        m: int, optional (default=200)
+            Evaluations outside of the top `m` have their predictions and estimators discarded to reduce memory usage.
+            The best `m` evaluations can be queried quickly through `n_best(int)`.
+            If set to `None` all evaluations remain sorted and with predictions and fit pipelines.
         prediction_sample: int or np.ndarray, optional (default=None)
             Allows downsampling of predictions to a select number before storing the evaluation.
             This is useful if you don't plan on using all predictions anyway, as it lowers memory usage.
@@ -50,16 +65,23 @@ class EvaluationLibrary:
             Set with an array if it matters which predictions to keep (e.g. class stratified samples).
         """
         self.top_evaluations = []
-        self._max_n_evaluations = max_number_of_evaluations
+        self.other_evaluations = []
+        self._m = m
         self._sample = prediction_sample
+
+    @property
+    def evaluations(self):
+        return self.top_evaluations + self.other_evaluations
 
     def save_evaluation(self, evaluation: Evaluation) -> None:
         self._downsample_predictions(evaluation)
 
-        if self._max_n_evaluations is None or self._max_n_evaluations > len(self.top_evaluations):
+        if self._m is None or self._m > len(self.top_evaluations):
             heapq.heappush(self.top_evaluations, evaluation)
         else:
-            heapq.heappushpop(self.top_evaluations, evaluation)
+            removed = heapq.heappushpop(self.top_evaluations, evaluation)
+            removed.predictions, removed.estimators = None, None
+            self.other_evaluations.append(removed)
 
     def _downsample_predictions(self, evaluation: Evaluation):
         """ Downsample predictions if possible and specified through `self._sample`. """
@@ -76,4 +98,8 @@ class EvaluationLibrary:
         evaluation.predictions = evaluation.predictions[self._sample]
 
     def n_best(self, n: int = 5) -> List[Evaluation]:
-        return [e for e in heapq.nlargest(n, self.top_evaluations) if e.predictions is not None]
+        """ Return the best `n` pipelines. Slower if `n` exceeds `m` given on initialization. """
+        if n <= self._m:
+            return [e for e in heapq.nlargest(n, self.top_evaluations) if e.predictions is not None]
+        else:
+            return list(reversed(sorted(self.evaluations)))[:n]
--- tests/unit/test_evaluation_library.py
+++ tests/unit/test_evaluation_library.py
@@ -1,5 +1,6 @@
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, Union
 import numpy as np
+import pandas as pd
 
 from gama.genetic_programming.components import Individual
 from gama.utilities.evaluation_library import Evaluation, EvaluationLibrary
@@ -8,30 +9,61 @@ from .unit_fixtures import pset, GaussianNB, RandomForestPipeline, LinearSVC
 
 def _mock_evaluation(
         individual: Individual,
+        predictions: Optional[Union[np.ndarray, pd.DataFrame, pd.Series]] = object(),
         score: Optional[Tuple[float, ...]] = None,
-        predictions: Optional[np.ndarray] = object(),
         estimators: List[object] = None,
         start_time: int = 0,
         duration: int = 0,
         error: str = None
 ) -> Evaluation:
-    evaluation = Evaluation(individual)
-    if score is not None:
-        evaluation.score = score
-    else:
-        evaluation.score = tuple(np.random.random(size=(3,)))
-    evaluation.predictions = predictions
-    evaluation.estimators = estimators
-    evaluation.start_time = start_time
-    evaluation.duration = duration
-    evaluation.error = evaluation.error
-    return evaluation
+    return Evaluation(
+        individual,
+        predictions,
+        score if score is not None else tuple(np.random.random(size=(3,))),
+        estimators,
+        start_time,
+        duration,
+        error
+    )
+
+
+def _test_subsample(sample, predictions, subsample):
+    """ Test the `predictions` correctly get sampled to `subsample`. """
+    lib = EvaluationLibrary(prediction_sample=sample)
+    best_evaluation = _mock_evaluation(GaussianNB, predictions=predictions)
+    lib.save_evaluation(best_evaluation)
+    assert best_evaluation.predictions.shape == subsample.shape, "Subsample does not have expected shape."
+    assert np.array_equal(best_evaluation.predictions, subsample), "Content of subsample differs from expected."
+
+
+def test_evaluation_convert_predictions_from_1darray_to_nparray(GaussianNB):
+    array = np.random.random(size=(30,))
+    assert isinstance(_mock_evaluation(GaussianNB, array).predictions, np.ndarray), "nparray converted"
+    assert _mock_evaluation(GaussianNB, array).predictions.shape == (30,), "Shape should not change."
+
+
+def test_evaluation_convert_predictions_from_2darray_to_nparray(GaussianNB):
+    array = np.random.random(size=(30, 5))
+    assert isinstance(_mock_evaluation(GaussianNB, array).predictions, np.ndarray), "nparray converted"
+    assert _mock_evaluation(GaussianNB, array).predictions.shape == (30, 5), "Shape should not change."
+
+
+def test_evaluation_convert_predictions_from_series_to_nparray(GaussianNB):
+    series = pd.Series(np.random.random(size=(30,)))
+    assert isinstance(_mock_evaluation(GaussianNB, series).predictions, np.ndarray), "Series not converted."
+    assert _mock_evaluation(GaussianNB, series).predictions.shape == (30,), "Shape should not change."
+
+
+def test_evaluation_convert_predictions_from_dataframe_to_nparray(GaussianNB):
+    dataframe = pd.DataFrame(np.random.random(size=(30, 5)))
+    assert isinstance(_mock_evaluation(GaussianNB, dataframe).predictions, np.ndarray), "Dataframe not converted."
+    assert _mock_evaluation(GaussianNB, dataframe).predictions.shape == (30, 5), "Shape should not change."
 
 
 def test_evaluation_library_max_number_evaluations(GaussianNB):
     """ Test `max_number_of_evaluations` correctly restricts the number of evaluations in `top_evaluations`. """
-    lib200 = EvaluationLibrary(max_number_of_evaluations=200, prediction_sample=None)
-    lib_unlimited = EvaluationLibrary(max_number_of_evaluations=None, prediction_sample=None)
+    lib200 = EvaluationLibrary(m=200, prediction_sample=None)
+    lib_unlimited = EvaluationLibrary(m=None, prediction_sample=None)
 
     worst_evaluation = _mock_evaluation(GaussianNB, score=(0., 0., 0.))
     lib200.save_evaluation(worst_evaluation)
@@ -49,7 +81,7 @@ def test_evaluation_library_max_number_evaluations(GaussianNB):
 
 def test_evaluation_library_n_best(GaussianNB):
     """ Test `max_number_of_evaluations` correctly restricts the number of evaluations in `top_evaluations`. """
-    lib = EvaluationLibrary(max_number_of_evaluations=None, prediction_sample=None)
+    lib = EvaluationLibrary(m=None, prediction_sample=None)
 
     best_evaluation = _mock_evaluation(GaussianNB, score=(1., 1., 1.))
     worst_evaluation = _mock_evaluation(GaussianNB, score=(0., 0., 0.))
@@ -63,3 +95,27 @@ def test_evaluation_library_n_best(GaussianNB):
     assert best_evaluation is lib.n_best(10)[0], "`best_evaluation` should be number one in `n_best`"
     assert worst_evaluation not in lib.n_best(10), "`worst_evaluation` should not be in the top 10 of 12 evaluations."
     assert len(lib.n_best(100)) == 12, "`n > len(lib.top_evaluations)` should return all evaluations."
+
+
+def test_evaluation_library_sample_np2d_prediction(GaussianNB):
+    """ Test `prediction_sample` set with np.ndarray correctly samples np.ndarray predictions with ndim=2. """
+    probabilities = np.random.random(size=(30, 5))
+    _test_subsample(sample=np.asarray([0, 1, 3]), predictions=probabilities, subsample=probabilities[[0, 1, 3], :])
+
+
+def test_evaluation_library_sample_pd2d_prediction(GaussianNB):
+    """ Test `prediction_sample` set with np.ndarray correctly samples pd.DataFrame predictions. """
+    probabilities = pd.DataFrame(np.random.random(size=(30, 5)))
+    _test_subsample(sample=np.asarray([0, 1, 3]), predictions=probabilities, subsample=probabilities.iloc[[0, 1, 3], :])
+
+
+def test_evaluation_library_sample_np1d_prediction(GaussianNB):
+    """ Test `prediction_sample` set with np.ndarray correctly samples np.ndarray predictions with ndim=1. """
+    probabilities = np.random.random(size=(30,))
+    _test_subsample(sample=np.asarray([0, 1, 3]), predictions=probabilities, subsample=probabilities[[0, 1, 3]])
+
+
+def test_evaluation_library_sample_pd1d_prediction(GaussianNB):
+    """ Test `prediction_sample` set with np.ndarray correctly samples pd.Series predictions. """
+    probabilities = pd.Series(np.random.random(size=(30,)))
+    _test_subsample(sample=np.asarray([0, 1, 3]), predictions=probabilities, subsample=probabilities.iloc[[0, 1, 3]])
