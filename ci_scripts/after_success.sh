8cd043c37d5a476c105af4c537ce2ab26359446c
PGijsbers <p.gijsbers@tue.nl>
2020-03-20 16:14:37 +0100

    readd exec permission

--- ci_scripts/after_success.sh
+++ ci_scripts/after_success.sh
@@ -4,6 +4,3 @@ if [ "$JOB" = "test" ]; then
   # codecov will merge reports automatically
   bash <(curl -s https://codecov.io/bash)
 fi
-if [ "$JOB" = "deploy" ]; then
-  sphinx-build -b html docs/source docs/$TRAVIS_BRANCH
-fi
