83af19c228f26745b642b23b797a5e9464e82cf5
PGijsbers <p.gijsbers@tue.nl>
2020-03-20 15:29:46 +0100

    change condition

--- ci_scripts/script.sh
+++ ci_scripts/script.sh
@@ -12,6 +12,6 @@ if [ "$JOB" = "test" ]; then
   pytest --cov=gama -sv -n 4 tests/"$SUITE"/
 fi
 if [ "$JOB" = "deploy" ]; then
-  pytest --cov=gama -sv -n 4 tests/unit/
+  pytest -sv -n 4 tests/unit/
   # pytest --cov=gama -sv -n 4 tests/system/
 fi
