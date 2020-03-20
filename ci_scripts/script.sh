1c210fdc97372c59f6bf45c741cb29d37643f7cf
PGijsbers <p.gijsbers@tue.nl>
2020-03-20 11:26:05 +0100

    Return exit code of tests

--- ci_scripts/script.sh
+++ ci_scripts/script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ "$JOB" = "check" ] || [ "$JOB" = "deploy" ]; then
-  pre-commit run --all-files
+if [ "$JOB" = "check" ]; then
+  exit $(pre-commit run --all-files)
 fi
 if [ "$JOB" = "test" ]; then
-  pytest --cov=gama -sv -n 4 tests/"$SUITE"/
+  exit $(pytest --cov=gama -sv -n 4 tests/"$SUITE"/)
 fi
 if [ "$JOB" = "deploy" ]; then
-  pytest --cov=gama -sv -n 4 tests/unit/
-  pytest --cov=gama -sv -n 4 tests/system/
+  if [ $(pre-commit run --all-files) ] || \
+     [ $(pytest --cov=gama -sv -n 4 tests/unit/) ] || \
+     [ $(pytest --cov=gama -sv -n 4 tests/system/) ]
+  then
+    exit $?
+  fi
 fi
