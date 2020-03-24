Added code to achieve correct behavior when the metric is F1 (average=binary)

gama/GamaClassifier.py
@@ -7,11 +7,47 @@ from d3m.container import DataFrame as D3MDataFrame
 from d3m.metadata import base as mdbase
 from sklearn.base import ClassifierMixin
 from sklearn.preprocessing import LabelEncoder
+from sklearn.utils import column_or_1d
 
 from .gama import Gama
 from gama.data import X_y_from_arff
 from gama.configuration.classification import clf_config
 from gama.utilities.metrics import scoring_to_metric
+
+
+# If the metric is standard F1 (and there's therefore a target, or 'pos', label), we need to ensure that it maps
+# to the value 1, since the F1 positive value defaults to 1.  This class implements that constraint, but should
+# behave in the same way as LabelEncoder in other respects.
+class SpecialLabelEncoder(object):
+
+    def __init__(self, pos_label):
+        self._pos_label = pos_label
+        self._fitted = False
+
+    def fit(self, y):
+        if self._fitted:
+            return
+        y = column_or_1d(y, warn=True)
+        self.classes_ = np.unique(y)
+        olabel = self.classes[1]
+        if self._pos_label is not None and olabel != self._pos_label:
+            for i, label in enumerate(self.classes):
+                if label == self._pos_label:
+                    self.classes[1], self.classes[i] = label, olabel
+                    break
+        # Create a reverse map
+        self.rclasses = dict((l, i) for i, l in enumerate(self.classes))
+        self._fitted = True
+        return self
+
+    def transform(self, y):
+        return np.array([self.rclasses[label] for label in y])
+
+    def fit_transform(self, y):
+        return self.fit(y).transform(y)
+
+    def inverse_transform(self, y):
+        return self.classes[np.asarray(y)]
 
 
 class GamaClassifier(Gama):
@@ -26,7 +62,7 @@ class GamaClassifier(Gama):
             # we don't want classifiers that do not have `predict_proba`, because then we have to
             # start doing one hot encodings of predictions etc.
             config = {alg: hp for (alg, hp) in config.items()
-                      if not (inspect.isclass(alg) and issubclass(alg, ClassifierMixin)and not hasattr(alg(), 'predict_proba'))}
+                      if not (inspect.isclass(alg) and issubclass(alg, ClassifierMixin) and not hasattr(alg(), 'predict_proba'))}
 
         self._label_encoder = None
         super().__init__(*args, **kwargs, config=config, scoring=scoring)
@@ -103,8 +139,10 @@ class GamaClassifier(Gama):
         X, _ = X_y_from_arff(arff_file_path)
         return self._predict_proba(X)
 
-    def fit(self, x, y, *args, **kwargs):
-        """ Should use base class documentation. """
+    def fit(self, x, y, pos_label=None, *args, **kwargs):
+        """ Should use base class documentation, with one exception: the pos_label parameter signals to
+        Gama that a binary F1 metric is being used and notifies it regarding the positive label.
+        """
         # If a D3M data frame, not the semantic types, so we can restore them below
         if isinstance(y, D3MDataFrame):
             y_ = y.squeeze()
@@ -115,8 +153,11 @@ class GamaClassifier(Gama):
         else:
             y_ = y
             col_semantic_types = None
-        self._label_encoder = LabelEncoder().fit(y_)
-        if any([isinstance(yi, str) for yi in y_]):
+        if pos_label is None:
+            self._label_encoder = LabelEncoder().fit(y_)
+        else:
+            self._label_encoder = SpecialLabelEncoder(pos_label).fit(y_)
+        if any([isinstance(yi, str) for yi in y_]) or pos_label is not None:
             # If target values are `str` we encode them or scikit-learn will complain.
             y = self._label_encoder.transform(y_)
             # D3M categorical predictors barf if metadata is absent
@@ -126,6 +167,7 @@ class GamaClassifier(Gama):
                     y.metadata = y.metadata.add_semantic_type((mdbase.ALL_ELEMENTS, 0), stype)
         super().fit(x, y, *args, **kwargs)
 
+    # Is this used??
     def _encode_labels(self, y):
         self._label_encoder = LabelEncoder().fit(y)
         return self._label_encoder.transform(y)