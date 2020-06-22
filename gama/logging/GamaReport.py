453642302121a341059b04d4624be20fa84df281
PGijsbers <p.gijsbers@tue.nl>
2020-06-22 19:33:54 +0200

    Add compatibility for ASHA with Dashboard
    Only feature lacking now is live updates.

--- gama/logging/GamaReport.py
+++ gama/logging/GamaReport.py
@@ -52,8 +52,6 @@ class GamaReport:
 
         self.incomplete = len(self.phases) < 3
 
-        # [x] Extract Phases
-        # [x] Extract Evaluations
         self.evaluations = evaluations_to_dataframe(
             os.path.join(log_directory, "evaluations.log"), self.metrics
         )
@@ -64,22 +62,11 @@ class GamaReport:
             for id_, pipeline in zip(self.evaluations.id, self.evaluations.pipeline)
         }
 
-        # [ ] Extract advanced setup (e.g. method information)
         # [ ] Allow for updates
         # [ ] Dashboard -- how point to directory??
 
-        # parse_method_data: Dict[str, Callable[..., pd.DataFrame]] = defaultdict(
-        #     lambda: lambda *args: None,
-        #     AsynchronousSuccessiveHalving=_ASHA_data_to_dataframe,
-        # )
-        # # search_method is formatted like NAME(kwargs)
-        # # where kwargs could contain additional parentheses.
-        # method_name, _ = self.search_method.split("(", maxsplit=1)
-        # method_token = METHOD_TOKENS.get(method_name)
-        # self.method_data = parse_method_data[method_name](
-        #     events_by_type[method_token], self.metrics
-        # )
-        self.method_data = None
+        self.search_method = self.hyperparameters["search_method"]
+        self.method_data = self.evaluations
 
     # def update(self) -> bool:
     #     new_lines = _find_new_lines(self._filename, start_from=self._lines_read)
@@ -117,24 +104,12 @@ class GamaReport:
 # new
 def init_to_hps(init_line: str) -> Dict[str, str]:
     all_arguments = init_line.split("(", maxsplit=1)[-1].rsplit(")", maxsplit=1)[0]
+    # A little hackery to get nested configurations (e.g. of search) to work
+    # only supports one nested level - will do proper parsing later
+    for token in ["()", "(", ")", ",,"]:
+        all_arguments = all_arguments.replace(token, ",")
+    print(all_arguments)
     return dict(hp.split("=") for hp in all_arguments.split(","))  # type: ignore
-
-
-# old
-
-# def _lines_to_dict(log_lines: List[str]):
-#     # Find the Parseable Log Events and discard their start/end tokens.
-#     ple_lines = [
-#         line.split(PLE_DELIM)[1:-1]
-#         for line in log_lines
-#         if line.startswith(PLE_START) and line.endswith(f"{PLE_END}")
-#     ]
-#
-#     events_by_type: Dict[str, List[List[str]]] = defaultdict(list)
-#     for token, *event in ple_lines:
-#         events_by_type[token].append(event)
-#
-#     return events_by_type
 
 
 # def _find_new_lines(logfile: str, start_from: int = 0) -> List[str]:
@@ -142,48 +117,6 @@ def init_to_hps(init_line: str) -> Dict[str, str]:
 #         log_lines = [line.rstrip() for line in fh.readlines()]
 #     new_lines = log_lines[start_from:]
 #     return new_lines
-
-
-# def _find_metric_configuration(
-#     init_lines: List[List[str]],
-# ) -> Tuple[List[str], str, str, str]:
-#     hps = init_lines[0][0].split(",")
-#
-#     def hyperparameter_value_of(hp_name: str) -> str:
-#         return [hp.split("=", maxsplit=1)[-1] for hp in hps if hp_name in hp][0]
-#
-#     metric = hyperparameter_value_of("scoring")
-#     regularize = hyperparameter_value_of("regularize_length")
-#     search = hyperparameter_value_of("search_method")
-#     filename = hyperparameter_value_of("keep_analysis_log")
-#     postprocessing = hyperparameter_value_of("post_processing_method")
-#     if bool(regularize):
-#         return [metric, "length"], search, postprocessing, filename
-#     else:
-#         return [metric], search, postprocessing, filename
-
-
-# def _find_phase_information(
-#     events_by_type: Dict[str, List[str]]
-# ) -> List[Tuple[str, str, datetime, float]]:
-#     """ For each phase (e.g. search), find the type (e.g. ASHA) and its duration. """
-#     phases = ["preprocessing", "search", "postprocess"]
-#     phase_info = []
-#     # Events as phase;algorithm;logtime
-#     for phase in phases:
-#         phase_starts = [e for e in events_by_type[TOKENS.PHASE_START] if phase in e]
-#         phase_ends = [e for e in events_by_type[TOKENS.PHASE_END] if phase in e]
-#         if phase_starts == [] or phase_ends == []:
-#             # The phase has either not yet started, or not yet completed.
-#             # Then this is also true for later phases.
-#             break
-#         start_phase, end_phase = phase_starts[0], phase_ends[0]
-#         _, _, start_time = start_phase
-#         start_time_dt = datetime.strptime(start_time, TIME_FORMAT)
-#         _, algorithm, end_time = end_phase
-#         duration = datetime.strptime(end_time, TIME_FORMAT) - start_time_dt
-#         phase_info.append((phase, algorithm, start_time_dt, duration.total_seconds()))
-#     return phase_info
 
 
 def evaluations_to_dataframe(file: str, metric_names: List[str]):
@@ -208,56 +141,3 @@ def evaluations_to_dataframe(file: str, metric_names: List[str]):
     else:
         df["relative_end"] = pd.Series()
     return df
-
-
-# def _evaluations_to_dataframe(
-#     evaluation_lines: List[List[str]],
-#     metric_names: Optional[List[str]] = None,
-#     search_start: datetime = None,
-#     start_n: int = 0,
-# ) -> pd.DataFrame:
-#     """ Create a dataframe with all parsed EVAL events in the log. """
-#     evaluations = []
-#     for i, line in enumerate(evaluation_lines, start=start_n):
-#         time, pid, duration, process_duration, fitness, id_, pipe_str, log_time = line
-#         # Fitness logged as '(metric1, metric2, ..., metriclast)'
-#         metrics_values = [float(value) for value in fitness[1:-1].split(",")]
-#         evaluations.append(
-#             [i, time, pid, float(duration), *metrics_values, pipe_str, id_]
-#         )
-#
-#     if metric_names is None:
-#         metric_names = [f"metric_{m_i}" for m_i in range(len(metrics_values))]
-#     column_names = ["n", "start", "pid", "duration", *metric_names, "pipeline", "id"]
-#     df = pd.DataFrame(evaluations, columns=column_names)
-#     for metric in metric_names:
-#         df[f"{metric}_cummax"] = df[metric].cummax()
-#
-#     df.start = pd.to_datetime(df.start)
-#     df.duration = pd.to_timedelta(df.duration, unit="s")
-#     search_start = search_start if search_start is not None else df.start.min()
-#     if len(df.start) > 0:
-#         df["relative_end"] = (
-#             (df.start + df.duration) - search_start
-#         ).dt.total_seconds()
-#     else:
-#         df["relative_end"] = pd.Series()
-#     return df
-#
-#
-# def _ASHA_data_to_dataframe(
-#     asha_lines: List[List[str]], metric_names: Optional[List[str]] = None
-# ) -> pd.DataFrame:
-#     asha_data = []
-#     for i, line in enumerate(asha_lines):
-#         rung, duration, fitness, id_, pipeline_str, log_time = line
-#         metrics_values = [float(value) for value in fitness[1:-1].split(",")]
-#         asha_data.append([i, rung, float(duration), *metrics_values, pipeline_str, id_])  # noqa: E501
-#
-#     if metric_names is None:
-#         metric_names = [f"metric_{m_i}" for m_i in range(len(metrics_values))]
-#     column_names = ["n", "rung", "duration", *metric_names, "pipeline", "id"]
-#     df = pd.DataFrame(asha_data, columns=column_names)
-#     df.duration = pd.to_timedelta(df.duration, unit="s")
-#
-#     return df
