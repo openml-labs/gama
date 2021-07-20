4c2a107228aac47cda26f93e0b2272f8adad7e35
PGijsbers <p.gijsbers@tue.nl>
2021-07-20 11:20:05 +0200

    Bugfixes (#120)
    * Re-use compile function when adding guard
    
    Makes using a custom compile function possible, otherwise an error is
    thrown because of a mismatch.
    
    * Fix incorrect check of empty list
    
    * Avoid invalid `max_pipeline_length` from being set
    
    whenever there are no preprocessing steps in the search space, the
    maximum length of a pipeline should be 1.
    
    * Avoid mut_replace_prim if no other prim available
    
    * Version bump with start release notes

--- gama/genetic_programming/mutation.py
+++ gama/genetic_programming/mutation.py
@@ -51,7 +51,7 @@ def mut_replace_primitive(individual: Individual, primitive_set: dict) -> None:
         return len(primitive_set[primitive._primitive.output]) > 1
 
     primitives = list(filter(primitive_replaceable, enumerate(individual.primitives)))
-    if primitives == 0:
+    if len(primitives) == 0:
         raise ValueError("Individual has no primitives suitable for replacement.")
 
     primitive_index, old_primitive_node = random.choice(primitives)
@@ -148,7 +148,12 @@ def random_valid_mutation_in_place(
             partial(mut_shrink, shrink_by=n_primitives - max_length)
         )
     else:
-        available_mutations.append(mut_replace_primitive)
+        replaceable_primitives = filter(
+            lambda p: len(primitive_set[p._primitive.output]) > 1, individual.primitives
+        )
+        if len(list(replaceable_primitives)) > 1:
+            available_mutations.append(mut_replace_primitive)
+
         if max_length is None or n_primitives < max_length:
             available_mutations.append(mut_insert)
         if n_primitives > 1:
