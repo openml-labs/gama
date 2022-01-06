0dda2bec4089dcd2c6b003908292f8e9056114cb
prabhant <prabhantsingh@gmail.com>
2022-01-06 01:33:54 +0100

    Auto stash before merge of "river-integration" and "origin/river-integration"

--- AutOL_plots.py
+++ AutOL_plots.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 
 # read the file into a list of lines and print to separate files for different outputs
+<<<<<<< Updated upstream
 for d in [4,5,6]:
     for version in ["basic", "ensemble", "modelstore"]:
         with open("/home/bcelik/Results_AutOL/"+version+"/results_"+version+"_"+str(d)+".txt",'r') as f:
@@ -34,11 +35,42 @@ for d in [4,5,6]:
         output_file_perf.close()
         output_file_retrainings.close()
         output_file_update.close()
+=======
+for d in [40,41]:
+    with open("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/results_ensemble_"+str(d)+".txt",'r') as f:
+        lines = f.read().split("\n")
+    output_file_perf = open("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Performance_Output_"+ str(d)+".txt", "w")
+    output_file_retrainings = open("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Retrainings_Output_"+ str(d)+".txt", "w")
+    output_file_update = open("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Update_Output_"+ str(d)+".txt", "w")
+
+
+    word_perf = 'Test batch'
+    word_drift = 'Change detected'
+    word_nondrift = 'No drift but'
+    word_update = 'Online model is updated'
+
+    # iterate over lines, and print out line numbers which contain the word of interest.
+    for j,line in enumerate(lines):
+        if word_perf in line: # or word in line.split() to search for full word
+            print("{}".format(line), file=output_file_perf)
+        if word_drift in line:
+            print("{}".format(line), file=output_file_retrainings)
+        if word_update in line:
+            print("{}".format(line), file=output_file_update)
+        if word_nondrift in line:
+            print("{}".format(line), file=output_file_retrainings)
+
+
+    output_file_perf.close()
+    output_file_retrainings.close()
+    output_file_update.close()
+>>>>>>> Stashed changes
 
 #Read data from the output files
 # Get test scores and batch numbers
 results={}
 retrainings={}
+<<<<<<< Updated upstream
 for d in [4, 5, 6]:  # data streams
     results["AutOL_Data_" + str(d)]={}
     retrainings["AutOL_Data_" + str(d)] = {}
@@ -61,6 +93,25 @@ for d in [4, 5, 6]:  # data streams
             retrainings["AutOL_Data_" + str(d)][version]['Model update'] = read_update.iloc[:, [6]]
 
 
+=======
+for d in [40, 41]:  # data streams
+    #Get performance
+    read_perf = pd.read_table("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Performance_Output_" + str(d) + ".txt", header=None, sep=' ')
+    results["AutOL_Ensemble_Data_" + str(d)] = (read_perf.iloc[:, [3, 6]])
+    results["AutOL_Ensemble_Data_" + str(d)].columns = ['Batch number', 'Test score']
+    results["AutOL_Ensemble_Data_" + str(d)]['Test score'] = results["AutOL_Ensemble_Data_" + str(d)]['Test score'].str.rstrip('%').astype('float') / 100.0
+
+    #Get retraining points and types - No : no drift training point, Change: drift training point
+    if os.path.getsize("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Retrainings_Output_" + str(d) + ".txt") > 0:
+        read_retraining = pd.read_table("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Retrainings_Output_" + str(d) + ".txt", header=None, sep=' ')
+        retrainings["AutOL_Ensemble_Data_" + str(d)] = read_retraining.iloc[:, [5,0]]
+        retrainings["AutOL_Ensemble_Data_" + str(d)].columns = ['Batch number', 'Type']
+
+    #Get model switch to ensemble or AutOL output
+    if os.path.getsize("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Retrainings_Output_" + str(d) + ".txt") > 0:
+        read_update = pd.read_table("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Update_Output_" + str(d) + ".txt", header=None, sep=' ')
+        retrainings["AutOL_Ensemble_Data_" + str(d)]['Model update'] = read_update.iloc[:, [6]]
+>>>>>>> Stashed changes
 
 #Plot-1 Performance
 
@@ -85,6 +136,7 @@ for d in [4, 5, 6]:
 
     ax.set_title('AutOL Prequential Performance - Data '+data_names[int(d/10)])
 
+<<<<<<< Updated upstream
     #Plot performance lines of each version
     # basic
     l0 = ax.plot(results["AutOL_Data_" + str(d)]['basic']['Batch number'],
@@ -99,6 +151,33 @@ for d in [4, 5, 6]:
                     results["AutOL_Data_" + str(d)]['modelstore']['Test score'],
                     label='AutOL - Model Store', lw=4, color='g')
 
+=======
+    l3 = ax.plot(results["AutOL_Ensemble_Data_" + str(d)]['Batch number'],
+                    results["AutOL_Ensemble_Data_" + str(d)]['Test score'],
+                    label='AutOL_Without_Extra_Training', lw=4, color='b')
+    l2 = ax.plot(results["AutOL_Ensemble_Data_" + str(d+1)]['Batch number'],
+                    results["AutOL_Ensemble_Data_" + str(d+1)]['Test score'],
+                    label='AutOL_With_Extra_Training', lw=4, color='r')
+    # l1 = ax[i].plot(results["Auto_"+str(i+1)]["Option_1"]["Data_"+str(d)]['Batch number'],results["Auto_"+str(i+1)]["Option_1"]["Data_"+str(d)]['Test score'],label = '1. Best model adaptation after drift', lw=3, color='g')
+
+    if os.path.getsize("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Retrainings_Output_"+ str(d)+".txt") > 0:
+        i=0
+        for xc in retrainings["AutOL_Ensemble_Data_" + str(d)]['Batch number']:
+            if retrainings["AutOL_Ensemble_Data_" + str(d)]['Type'].iloc[i] == "Change":
+                ax.axvline(x=xc, linestyle=':', color='b')
+            else:
+                ax.axvline(x=xc, linestyle='solid', color='b')
+            i=i+1
+
+    if os.path.getsize("/Users/prabhant/OpenML/gama/Results_AutOL/Grace/Retrainings_Output_"+ str(d+1)+".txt") > 0:
+        i=0
+        for xc in retrainings["AutOL_Ensemble_Data_" + str(d+1)]['Batch number']:
+            if retrainings["AutOL_Ensemble_Data_" + str(d+1)]['Type'].iloc[i] == "Change":
+                ax.axvline(x=xc, linestyle=':', color='r')
+            else:
+                ax.axvline(x=xc, linestyle='solid', color='r')
+            i=i+1
+>>>>>>> Stashed changes
 
     handles, labels = ax.get_legend_handles_labels()
     plt.legend(handles, labels,
@@ -112,6 +191,7 @@ for d in [4, 5, 6]:
 
     plt.plot()
     figs[d].show()
+<<<<<<< Updated upstream
     figs[d].savefig("/home/bcelik/Results_AutOL/Performance_Data"+str(d)+".png")
 
 
@@ -135,3 +215,6 @@ for d in [4, 5, 6]:
     #         else:
     #             ax.axvline(x=xc, linestyle='solid', color='r')
     #         i=i+1
+=======
+    figs[d].savefig("/Users/prabhant/OpenML/gama/Results_AutOL/Performance_Data"+str(d)+".png")
+>>>>>>> Stashed changes
