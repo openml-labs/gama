openml-labs/gama

commit 24b1986e33790e0890e798a7ca57ec7029063b0d

Generate new ind late as possible (#140)
and don't allow errored-individuals into the population.

gama/search_methods/async_ea.py
@@ -122,14 +122,17 @@ def async_ea(
                 n_evaluated_individuals < max_n_evaluations
             ):
                 future = ops.wait_next(async_)
-                if future.exception is None:
-                    individual = future.result.individual
-                    current_population.append(individual)
+                if future.exception is None and future.result.error is None:
+                    current_population.append(future.result.individual)
                     if len(current_population) > max_pop_size:
                         to_remove = ops.eliminate(current_population, 1)
                         current_population.remove(to_remove[0])
 
-                if len(current_population) > 2:
+                if async_.job_queue_size <= 3:
+                    # Technically 0 should work to keep near-100% worker load,
+                    # increasing the number decreases the risk of lost compute time,
+                    # but also increases information lag. An offspring created too
+                    # early might miss out on a better parent.
                     new_individual = ops.create(current_population, 1)[0]
                     async_.submit(ops.evaluate, new_individual)
 

gama/utilities/generic/async_evaluator.py
@@ -78,7 +78,7 @@ class AsyncEvaluator:
 
     def __init__(
         self,
-        n_workers: Optional[int] = None,
+        n_workers: int = 1,
         memory_limit_mb: Optional[int] = None,
         logfile: Optional[str] = None,
         wait_time_before_forced_shutdown: int = 10,
@@ -86,9 +86,8 @@ class AsyncEvaluator:
         """
         Parameters
         ----------
-        n_workers : int, optional (default=None)
+        n_workers : int (default=1)
             Maximum number of subprocesses to run for parallel evaluations.
-            Defaults to `AsyncEvaluator.n_jobs`, using all cores unless overwritten.
         memory_limit_mb : int, optional (default=None)
             The maximum number of megabytes that this process and its subprocesses
             may use in total. If None, no limit is enforced.
@@ -109,9 +108,17 @@ class AsyncEvaluator:
         self._logfile = logfile
         self._wait_time_before_forced_shutdown = wait_time_before_forced_shutdown
 
+        # queue.qsize() may raise an error on Unix-like,
+        # more accurate results may be obtained by using a multiprocessing.Value
+        # but it adds a point of failure and I hope to replace this async
+        # module soon, so we use this approximation.
+        # Since we assume all workers will take up a job, we start below 0:
+        self.job_queue_size = -n_workers
+
         self._input: multiprocessing.Queue = multiprocessing.Queue()
         self._output: multiprocessing.Queue = multiprocessing.Queue()
         self._command: multiprocessing.Queue = multiprocessing.Queue()
+
         pid = os.getpid()
         self._main_process = psutil.Process(pid)
 
@@ -147,6 +154,7 @@ class AsyncEvaluator:
         self.clear_queue(self._input)
         self.clear_queue(self._output)
         self.clear_queue(self._command)
+        self.job_queue_size = -self._n_jobs
 
         # Even processes which 'stop' need to be 'waited',
         # otherwise they become zombie processes.
@@ -157,13 +165,13 @@ class AsyncEvaluator:
                 pass
         return False
 
-    def clear_queue(self, queue: multiprocessing.Queue):
-        while not queue.empty():
+    def clear_queue(self, q: multiprocessing.Queue):
+        while not q.empty():
             try:
-                queue.get(timeout=0.001)
-            except:
+                q.get(timeout=0.001)
+            except queue.Empty:
                 pass
-        queue.close()
+        q.close()
 
     def submit(self, fn: Callable, *args, **kwargs) -> AsyncFuture:
         """ Submit fn(*args, **kwargs) to be evaluated on a subprocess.
@@ -186,6 +194,7 @@ class AsyncEvaluator:
         future = AsyncFuture(fn, *args, **kwargs)
         self.futures[future.id] = future
         self._input.put(future)
+        self.job_queue_size += 1
         return future
 
     def wait_next(self, poll_time: float = 0.05) -> AsyncFuture:
@@ -216,6 +225,7 @@ class AsyncEvaluator:
 
             try:
                 completed_future = self._output.get(block=False)
+                self.job_queue_size -= 1
             except queue.Empty:
                 time.sleep(poll_time)
                 continue

commit 1c744715a3436de3170d9328ad8be0887e6f265c

Fix small error introduced by #140 (#145)
* Decrease job queue

* Decrease job count by 1 when killing process

Because the individual is already taken from the input queue.
Otherwise this would lead to a number of "ghost jobs" that are assumed
to be evaluated even though the process was killed.

gama/search_methods/async_ea.py
@@ -128,9 +128,11 @@ def async_ea(
                         to_remove = ops.eliminate(current_population, 1)
                         current_population.remove(to_remove[0])
 
-                if async_.job_queue_size <= 3:
+                if async_.job_queue_size <= 1:
                     # Technically 0 should work to keep near-100% worker load,
-                    # increasing the number decreases the risk of lost compute time,
+                    # especially if the dataset is sufficiently large to require
+                    # significant time to evaluate a pipeline.
+                    # Increasing the number decreases the risk of lost compute time,
                     # but also increases information lag. An offspring created too
                     # early might miss out on a better parent.
                     new_individual = ops.create(current_population, 1)[0]

gama/utilities/generic/async_evaluator.py
@@ -255,6 +255,7 @@ class AsyncEvaluator:
         """ Terminate a new worker node and remove it from the process pool. """
         process.terminate()
         process.wait(timeout=60)
+        self.job_queue_size -= 1
         self._processes.remove(process)
 
     def _control_memory_usage(self, threshold=0.05):