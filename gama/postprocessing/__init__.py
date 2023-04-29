fc16565ff93caa76308edeca9fdaa3d00ca83071
PGijsbers <p.gijsbers@tue.nl>
2023-04-29 20:45:10 +0200

    Add to_code stub

--- gama/postprocessing/__init__.py
+++ gama/postprocessing/__init__.py
@@ -1,3 +1,7 @@
+from typing import Optional, Sequence, Tuple
+
+from sklearn.base import TransformerMixin
+
 from gama.postprocessing.base_post_processing import BasePostProcessing
 from gama.postprocessing.best_fit import BestFitPostProcessing
 from gama.postprocessing.ensemble import EnsemblePostProcessing
@@ -5,6 +9,13 @@ from gama.postprocessing.ensemble import EnsemblePostProcessing
 
 class NoPostProcessing(BasePostProcessing):
     """Does nothing, no time will be reserved for post-processing."""
+
+    def to_code(
+        self, preprocessing: Optional[Sequence[Tuple[str, TransformerMixin]]] = None
+    ) -> str:
+        raise NotImplementedError(
+            "NoPostProcessing has no `to_code` function, since no model is selected."
+        )
 
     def __init__(self, time_fraction: float = 0.0):
         super().__init__(time_fraction)
